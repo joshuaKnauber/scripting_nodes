9507f0f94fa603d265deceeff774b4a36a7bebb0
Joshua Knauber <joshua.knauber@gmail.com>
2020-04-27 14:24:07 +0200

    added addon properties to ndoes tree

--- node_categories.py
+++ node_categories.py
@@ -9,6 +9,13 @@ class ScriptingNodesTree(bpy.types.NodeTree):
     bl_label = "Scripting Nodes"
     bl_icon = 'SCRIPT'
 
+    addon_name: bpy.props.StringProperty(default="New Addon",name="Name",description="Name of your addon")
+    addon_author: bpy.props.StringProperty(default="",name="Author",description="Your name")
+    addon_description: bpy.props.StringProperty(default="",name="Description",description="Description of what your addon does")
+    addon_location: bpy.props.StringProperty(default="",name="Location",description="Location of your addon in blender")
+    addon_warning: bpy.props.StringProperty(default="",name="Warning",description="Warning message for your addon")
+    addon_category: bpy.props.StringProperty(default="General",name="Category",description="Category of your addon")
+
 
 class SN_ScriptingNodesCategory(NodeCategory):
     @classmethod
