252b16095bc3ab9dee8734aaedf2d591b72fd652
Joshua Knauber <joshua.knauber@gmail.com>
2020-04-28 15:08:25 +0200

    added function to create text file for addon

--- compile/compiler.py
+++ compile/compiler.py
@@ -3,13 +3,19 @@ import bpy
 class ScriptingNodesCompiler():
 
     def __init__(self):
-        self._registered_functions = []
-        self._registered_operators = []
-        self._registered_panels = []
+        self._functions = []
+        self._operators = []
+        self._panels = []
 
     def _is_scripting_tree(self):
         if bpy.context.space_data.tree_type == 'ScriptingNodesTree':
             return bpy.context.space_data.node_tree != None
+
+    def _reset(self):
+        pass
+
+    def _compile_functions(self, tree):
+        pass
 
     def _compile_operators(self, tree):
         operator_starts = []
@@ -17,10 +23,24 @@ class ScriptingNodesCompiler():
             if node.bl_idname == "":
                 operator_starts.append(node)
 
-    def _reset(self):
-        self._registered_functions = []
-        self._registered_operators = []
-        self._registered_panels = []
+    def _compile_interface(self, tree):
+        pass
+
+    def _create_file(self, tree):
+        name = tree.addon_name.lower().replace(" ","_") + ".py"
+
+        if not name in bpy.data.texts:
+            bpy.data.texts.new(name=name)
+        text = bpy.data.texts[name]
+
+        text.clear()
+
+        text.write("print('test')")
+
+        return text
+
+    def _register_file(self, addon):
+        pass
 
     def recompile(self):
         tree = bpy.context.space_data.node_tree
@@ -28,6 +48,12 @@ class ScriptingNodesCompiler():
 
             self._reset()
 
-            function_starts = []
+            self._compile_functions(tree)
             
             self._compile_operators(tree)
+
+            self._compile_interface(tree)
+
+            addon = self._create_file(tree)
+
+            self._register_file(addon)
