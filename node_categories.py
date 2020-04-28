5e70600b652177904bbbc3dd233000a33bfefda2
Joshua Knauber <joshua.knauber@gmail.com>
2020-04-28 15:53:39 +0200

    added basic addon info for compile

--- node_categories.py
+++ node_categories.py
@@ -14,8 +14,10 @@ class ScriptingNodesTree(bpy.types.NodeTree):
     addon_author: bpy.props.StringProperty(default="",name="Author",description="Your name")
     addon_description: bpy.props.StringProperty(default="",name="Description",description="Description of what your addon does")
     addon_location: bpy.props.StringProperty(default="",name="Location",description="Location of your addon in blender")
+    addon_wiki: bpy.props.StringProperty(default="",name="Wiki",description="Link to the documentation of your addon")
     addon_warning: bpy.props.StringProperty(default="",name="Warning",description="Warning message for your addon")
     addon_category: bpy.props.StringProperty(default="General",name="Category",description="Category of your addon")
+    addon_blender: bpy.props.IntVectorProperty(default=bpy.app.version,min=0,name="Version",description="Required blender version for your addon")
     addon_version: bpy.props.IntVectorProperty(default=(1, 0, 0),min=0,name="Version",description="Version of your addon")
 
     compiler = ScriptingNodesCompiler()
