678aaa299f3476608d51f4d93c15ee089540d66e
Joshua Knauber <joshua.knauber@gmail.com>
2020-06-14 00:16:32 +0200

    test commit

--- utitlity_functions.py
+++ utitlity_functions.py
@@ -5,4 +5,4 @@ def to_lower_camelcase(text):
         text[i] = word[0].upper() + word[1:] 
     text = ("").join(text)
     text = text[0].lower() + text[1:] 
-    return text  
+    return text   
