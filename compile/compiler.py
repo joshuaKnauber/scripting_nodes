f535034ca00b7fdafd6f1f92e8415ffa0dbd8515
Joshua Knauber <joshua.knauber@gmail.com>
2020-06-14 14:07:55 +0200

    added basic functions for comiler

--- compile/compiler.py
+++ compile/compiler.py
@@ -2,28 +2,71 @@ import bpy
 
 class ScriptingNodesCompiler():
 
+    # CALLABLE FUNCTIONS
+    # compile_active: takes the active node tree and compiles it after unregistering
+    # autocompile_active: compiles the active node tree if autocompile is active
+    # unregister_active: unregisters the active node tree
+    # unregister_all: unregisters all registered node trees
+
     def __init__(self):
         self._indents = 4 # the number of indents that should be used in the generated files
         self._modules = [] # the currently registered modules
 
-    def autocompile(self):
-        """ runs compile if the auto comile is enabled """
-        if bpy.context.scene.sn_properties.auto_compile:
-            self.recompile()
+    def _create_addon_text(self, tree):
+        """ creates the text for the addon with the given tree """
+        text = bpy.data.texts.new(".test")
+        return text
+
+    def _create_module(self, tree):
+        """ generates the code from the node tree and adds it as a module """
+        text = self._create_addon_text( tree )
+        module = {
+            "node_tree": tree,
+            "text": text,
+            "module": text.as_module()
+        }
+        self._modules.append(module)
+
+    def _register_tree(self, tree):
+        """ finds the matching module and registers it """
+        for module in self._modules:
+            if module["tree"] == tree:
+                module["module"].register()
 
     def _unregister_tree(self, tree):
-        """ unregisters the module if already registered """
+        """ unregisters the module if already registered and removes it """
         for module in self._modules:
             if module[ "node_tree" ] == tree:
                 module[ "module" ].unregister()
+                bpy.data.texts.remove(module["text"])
+                self._modules.remove(module)
+                break
 
-    def recompile(self):
+    def _recompile(self, tree):
         """ compiles the active node tree """
-        tree = bpy.context.space_data.node_tree
-
-        #self._unregister_tree( tree )
+        self._unregister_tree(tree)
+        self._create_module(tree)
+        self._register_tree(tree)
 
         bpy.context.area.tag_redraw()
+
+    def compile_active(self):
+        """ recompiles the active tree """
+        self._recompile(bpy.context.space_data.node_tree)
+
+    def unregister_active(self):
+        """ unregisters the active node trees module and removes it """
+        self._unregister_tree(bpy.context.space_data.node_tree)
+
+    def unregister_all(self):
+        """ unregisters all registered node trees """
+        for module in self._modules:
+            self._unregister_tree(module["tree"])
+
+    def autocompile_active(self):
+        """ runs compile if the auto comile is enabled """
+        if bpy.context.scene.sn_properties.auto_compile:
+            self.compile_active()
 
 
 global_compiler = ScriptingNodesCompiler()
--- properties/groups/addon_properties.py
+++ properties/groups/addon_properties.py
@@ -2,4 +2,8 @@ import bpy
 
 class ScriptingNodesProperties(bpy.types.PropertyGroup):
 
-    pass
+    def update_auto_compile(self,context):
+        """ function for updating the auto compile property """
+
+    # if true the node tree gets recompiled on changes
+    auto_compile: bpy.props.BoolProperty(default=False, name="Auto Reload", description="Automatically reloads the addon when changes are made", update=update_auto_compile)
