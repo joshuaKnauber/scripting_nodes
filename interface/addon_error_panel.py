c721bbe46a966dc38be0f590f09e2635a5f8850e
Joshua Knauber <joshua.knauber@gmail.com>
2020-06-15 22:42:59 +0200

    error handler and display

--- interface/addon_error_panel.py
+++ interface/addon_error_panel.py
@@ -1,5 +1,7 @@
 import bpy
+from textwrap import wrap
 from ..compile.compiler import compiler
+from ..handler.error_message_handler import ErrorMessageHandler
 
 
 class SN_PT_ErrorLogPanel(bpy.types.Panel):
@@ -11,49 +13,41 @@ class SN_PT_ErrorLogPanel(bpy.types.Panel):
     bl_region_type = 'UI'
     bl_category = "Visual Scripting"
 
+    error_message_handler = ErrorMessageHandler()
+
     @classmethod
     def poll(cls, context):
         return bool(compiler().get_active_addons_errors())
 
-    def draw_error(self, layout, error_type, error_message, fatal, node):
+    def draw_error(self, layout, error):
         box = layout.box()
         col = box.column(align=True)
 
         row = col.row()
-        if node != "":
-            row.operator("scripting_nodes.find_error_node",text="",emboss=False,icon="NODE").node_name = node
+        if error["node"]:
+            row.operator("scripting_nodes.find_error_node",text="",emboss=False,icon="NODE").node_name = error["node"].name
 
-        row.alert = fatal
-        row.label(text=error_type)
+        row.alert = self.error_message_handler.error_fatal(error["error"])
+        row.label(text=self.error_message_handler.error_name(error["error"]))
 
-        line_length = bpy.context.region.width // sn_props().line_width
-        error_message = wrap(error_message,line_length)
+        line_length = bpy.context.region.width // bpy.context.scene.sn_properties.line_width
+        error_message = wrap(self.error_message_handler.error_message(error["error"]),line_length)
 
         for line in error_message:
             col.label(text=line)
 
     def draw_header(self,context):
-        pass#self.layout.prop(sn_props(),"show_line_width",text="",icon="PREFERENCES",emboss=False)
+        self.layout.prop(context.scene.sn_properties,"show_line_width",text="",icon="PREFERENCES",emboss=False)
 
     def draw(self, context):
         layout = self.layout
         errors = compiler().get_active_addons_errors()
-        for error in errors:
-            layout.label(text=error["error"])
-        """
-        if sn_props().show_line_width:
+        
+        if context.scene.sn_properties.show_line_width:
             box = layout.box()
-            box.prop(sn_props(),"line_width",slider=True,text="Line breaks")
+            box.prop(context.scene.sn_properties,"line_width",slider=True,text="Line breaks")
 
         column = layout.column(align=False)
 
-        if len(error_props()) > 0:
-            for error in error_props():
-                if error.fatal_error:
-                    self.draw_error(column, error.error_type, error.error_message, True, error.node)
-            for error in error_props():
-                if not error.fatal_error:
-                    self.draw_error(column, error.error_type, error.error_message, False, error.node)
-        else:
-            column.label(text="No errors found")
-        """
+        for error in errors:
+            self.draw_error(column, error)
