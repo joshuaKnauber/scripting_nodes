a49ae21cfcf00a17367e654bfd94c26e110928ea
Joshua Knauber <joshua.knauber@gmail.com>
2020-06-15 19:31:22 +0200

    added panels

---
+++ interface/addon_error_panel.py
@@ -0,0 +1,58 @@
+import bpy
+
+
+
+class SN_PT_ErrorLogPanel(bpy.types.Panel):
+    """Creates a panel for displaying error messages in the node editors sidebar"""
+    bl_label = "Errors"
+    bl_order = 2
+    bl_idname = "SN_PT_ErrorLogPanel"
+    bl_space_type = 'NODE_EDITOR'
+    bl_region_type = 'UI'
+    bl_category = "Visual Scripting"
+
+    @classmethod
+    def poll(cls, context):
+        if context.space_data.tree_type == 'ScriptingNodesTree':
+            return context.space_data.node_tree != None
+
+
+    def draw_error(self, layout, error_type, error_message, fatal, node):
+        box = layout.box()
+        col = box.column(align=True)
+
+        row = col.row()
+        if node != "":
+            row.operator("scripting_nodes.find_error_node",text="",emboss=False,icon="NODE").node_name = node
+
+        row.alert = fatal
+        row.label(text=error_type)
+
+        line_length = bpy.context.region.width // sn_props().line_width
+        error_message = wrap(error_message,line_length)
+
+        for line in error_message:
+            col.label(text=line)
+
+    def draw_header(self,context):
+        self.layout.prop(sn_props(),"show_line_width",text="",icon="PREFERENCES",emboss=False)
+
+    def draw(self, context):
+        layout = self.layout
+        """
+        if sn_props().show_line_width:
+            box = layout.box()
+            box.prop(sn_props(),"line_width",slider=True,text="Line breaks")
+
+        column = layout.column(align=False)
+
+        if len(error_props()) > 0:
+            for error in error_props():
+                if error.fatal_error:
+                    self.draw_error(column, error.error_type, error.error_message, True, error.node)
+            for error in error_props():
+                if not error.fatal_error:
+                    self.draw_error(column, error.error_type, error.error_message, False, error.node)
+        else:
+            column.label(text="No errors found")
+        """
---
+++ interface/addon_info_panel.py
@@ -0,0 +1,52 @@
+import bpy
+
+
+class SN_PT_AddonInfoPanel(bpy.types.Panel):
+    """Creates a panel that lets you edit the Addon Info for the current NodeTree"""
+    bl_label = "Addon Info"
+    bl_order = 1
+    bl_idname = "SN_PT_AddonInfoPanel"
+    bl_space_type = 'NODE_EDITOR'
+    bl_region_type = 'UI'
+    bl_category = "Visual Scripting"
+
+    @classmethod
+    def poll(cls, context):
+        if context.space_data.tree_type == 'ScriptingNodesTree':
+            return context.space_data.node_tree != None
+
+    def draw(self, context):
+        layout = self.layout
+        col = layout.column(align=True)
+        col.label(text="Name")
+        col.prop(context.space_data.node_tree, "addon_name", text="")
+        col.separator()
+        col.label(text="Author")
+        col.prop(context.space_data.node_tree, "addon_author", text="")
+        col.separator()
+        col.label(text="Description")
+        col.prop(context.space_data.node_tree, "addon_description", text="")
+        col.separator()
+        col.label(text="Location")
+        col.prop(context.space_data.node_tree, "addon_location", text="")
+        col.separator()
+        col.label(text="Wiki")
+        col.prop(context.space_data.node_tree, "addon_wiki", text="")
+        col.separator()
+        col.label(text="Warning")
+        col.prop(context.space_data.node_tree, "addon_warning", text="")
+        col.separator()
+        col.label(text="Category")
+        col.prop(context.space_data.node_tree, "addon_category", text="")
+        col.separator()
+        col.label(text="Blender version")
+        row = col.row()
+        row.prop(context.space_data.node_tree, "addon_blender", text="")
+        col.label(text="Addon version")
+        row = col.row()
+        row.prop(context.space_data.node_tree, "addon_version", text="")
+
+        layout.separator()
+        layout.prop(context.space_data.node_tree,"compile_on_start")
+
+
