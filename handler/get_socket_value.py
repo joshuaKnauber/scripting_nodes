45c6a64a9932eba7a73a45169fdcedb7c65907ff
Joshua Knauber <joshua.knauber@gmail.com>
2020-06-15 20:21:15 +0200

    as list for get socket value as well as in and outputs

--- handler/get_socket_value.py
+++ handler/get_socket_value.py
@@ -6,23 +6,55 @@ class SocketHandler():
     # CALLABLE FUNCTIONS
     # socket_value: takes a socket and returns the code and errors for that socket
 
+    def _handle_input_socket(self, socket, socket_types):
+        """ returns the connected socket as well as errors """
+        value = []
+        errors = []
+        if socket.links[0].from_node.bl_idname != "NodeReroute":
+            if socket.links[0].from_socket.bl_idname in socket_types:
+                value = [socket.links[0].from_socket]
+            else:
+                errors.append("wrong_socket")
+        else:
+            current_socket = socket.links[0]
+            while current_socket.from_node.bl_idname == "NodeReroute":
+                if len(current_socket.from_node.links) > 1:
+                    current_socket = current_socket.links[0]
+                else:
+                    errors.append("no_connection")
+            value = [current_socket.from_socket]
+        return value, errors
+
+
+    def _handle_output_socket(self, socket, socket_types):
+        """ returns the connected socket as well as errors """
+        value = []
+        errors = []
+        if socket.links[0].to_node.bl_idname != "NodeReroute":
+            if socket.links[0].to_socket.bl_idname in socket_types:
+                value = [socket.links[0].to_socket]
+            else:
+                errors.append("wrong_socket")
+        else:
+            current_socket = socket.links[0]
+            while current_socket.to_node.bl_idname == "NodeReroute":
+                if len(current_socket.to_node.links) > 1:
+                    current_socket = current_socket.links[0]
+                else:
+                    errors.append("no_connection")
+            value = [current_socket.to_socket]
+        return value, errors
+
+
     def _handle_socket_connection(self, socket, socket_types):
+        """ returns the connected socket for in and outputs, as well as errors """
         errors = []
 
         if socket.is_linked:
-            if socket.links[0].from_node.bl_idname != "NodeReroute":
-                if socket.links[0].from_socket.bl_idname in socket_types:
-                    value = [socket.links[0].from_socket]
-                else:
-                    errors.append("wrong_socket")
+            if socket in socket.node.inputs:
+                value, errors = self._handle_input_socket(socket, socket_types)
             else:
-                current_socket = socket.links[0]
-                while current_socket.from_node.bl_idname == "NodeReroute":
-                    if len(current_socket.from_node.links) > 1:
-                        current_socket = current_socket.links[0]
-                    else:
-                        errors.append("no_connection")
-                value = [current_socket.from_socket]
+                value, errors = self._handle_output_socket(socket, socket_types)
         else:
             value = []
             errors.append("no_connection")
@@ -100,19 +132,25 @@ class SocketHandler():
 
         return value, errors
 
-    def _get_layout(self, socket):
+    def _get_layout(self, socket, as_list):
         """ gets the code of a layout socket """
         
         value, errors = self._handle_socket_connection(self, socket, ["SN_LayoutSocket"])
 
-        return value, errors
+        if as_list or not value:
+            return value, errors
+        else:
+            return value[0], errors
 
-    def _get_program(self, socket):
+    def _get_program(self, socket, as_list):
         """ gets the code of a program socket """
         
         value, errors = self._handle_socket_connection(self, socket, ["SN_ProgramSocket"])
 
-        return value, errors
+        if as_list or not value:
+            return value, errors
+        else:
+            return value[0], errors
 
     def _get_scene_data(self, socket):
         """ gets the code of a scene data socket """
@@ -121,7 +159,7 @@ class SocketHandler():
 
         return value, errors
 
-    def socket_value(socket):
+    def socket_value(socket,as_list=False):
         """ returns the code and errors for that socket """
         socket_type = socket.bl_idname
         
@@ -144,10 +182,10 @@ class SocketHandler():
             return self._get_enum(socket)
         
         elif socket_type == "SN_LayoutSocket":
-            return self._get_layout(socket)
+            return self._get_layout(socket, as_list)
         
         elif socket_type == "SN_ProgramSocket":
-            return self._get_program(socket)
+            return self._get_program(socket, as_list)
         
         elif socket_type == "SN_SceneDataSocket":
             return self._get_scene_data(socket)
