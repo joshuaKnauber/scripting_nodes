710dd1899212e2fef367f16a50b8ada05392c8e5
Joshua Knauber <joshua.knauber@gmail.com>
2020-06-21 00:00:47 +0200

    examples

--- interface/node_header.py
+++ interface/node_header.py
@@ -6,3 +6,6 @@ def node_header(self, context):
             row.operator("scripting_nodes.unregister_active", text="", icon="UNLINKED")
             row.separator()
             row.prop(context.scene.sn_properties,"auto_compile")
+            row.separator()
+    if context.space_data.tree_type == "ScriptingNodesTree":
+        self.layout.prop(context.scene.sn_properties,"examples",text="")
