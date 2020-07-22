e039e4041389f6f25921db4ad72ab59435b03eb5
BuildTools <finn.knauber@gmail.com>
2020-07-22 13:17:50 +0200

    added split node

--- nodes/interface/split.py
+++ nodes/interface/split.py
@@ -1,23 +1,24 @@
-#SN_LabelNode
+#SN_SplitNode
 
 import bpy
 from ...node_tree.base_node import SN_ScriptingBaseNode
 
 
-class SN_LabelNode(bpy.types.Node, SN_ScriptingBaseNode):
+class SN_SplitNode(bpy.types.Node, SN_ScriptingBaseNode):
 
-    bl_idname = "SN_LabelNode"
-    bl_label = "Label"
-    bl_icon = "SORTALPHA"
+    bl_idname = "SN_SplitNode"
+    bl_label = "Split"
+    bl_icon = "UV_ISLANDSEL"
     node_color = (0.89,0.6,0)
     should_be_registered = False
 
     def inititialize(self,context):
         self.sockets.create_input(self,"LAYOUT","Layout")
-        self.sockets.create_input(self,"STRING","Text")
-
-    def draw_buttons(self,context,layout):
-        self.draw_icon_chooser(layout)
+        factor = self.sockets.create_input(self, "FLOAT", "Factor")
+        factor.set_value(0.5)
+        factor.use_factor = True
+        self.sockets.create_output(self,"LAYOUT","Layout")
+        self.sockets.create_output(self,"LAYOUT","Layout")
 
     def evaluate(self, socket, input_data, errors):
         return {
@@ -34,3 +35,6 @@ class SN_LabelNode(bpy.types.Node, SN_ScriptingBaseNode):
 
     def required_imports(self):
         return []
+
+    def layout_type(self):
+        return "split"
