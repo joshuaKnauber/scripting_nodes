a0b24a0cb4116178d465fe760deccd6fe9b3f839
BuildTools <finn.knauber@gmail.com>
2020-07-22 14:41:50 +0200

    added negate node

--- nodes/logic/negate.py
+++ nodes/logic/negate.py
@@ -1,26 +1,21 @@
-#SN_AndOrNode
+#SN_NegateNode
 
 import bpy
 from ...node_tree.base_node import SN_ScriptingBaseNode
 
 
-class SN_AndOrNode(bpy.types.Node, SN_ScriptingBaseNode):
+class SN_NegateNode(bpy.types.Node, SN_ScriptingBaseNode):
 
-    bl_idname = "SN_AndOrNode"
-    bl_label = "And/Or"
-    bl_icon = "MEMORY"
+    bl_idname = "SN_NegateNode"
+    bl_label = "Negate"
+    bl_icon = "FORCE_CHARGE"
     node_color = (0.65,0,0)
+    bl_width_default = 110
     should_be_registered = False
-
-    andOr: bpy.props.EnumProperty(items=[("and", "And", "Both need to be true"), ("or", "Or", "At least one needs to be true")],name="Operation", description="The operation you want to commence")
 
     def inititialize(self,context):
         self.sockets.create_input(self,"BOOLEAN","Value")
-        self.sockets.create_input(self,"BOOLEAN","Value")
         self.sockets.create_output(self,"BOOLEAN","Output")
-
-    def draw_buttons(self, context, layout):
-        layout.prop(self, "andOr", text="")
 
     def evaluate(self, socket, input_data, errors):
         return {
