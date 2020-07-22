22229fefa587ea0070dc3622f4aa83ca1d86579a
BuildTools <finn.knauber@gmail.com>
2020-07-22 15:04:56 +0200

    added combine text node

---
+++ nodes/logic/combine_text.py
@@ -0,0 +1,31 @@
+#SN_CombineTextNode
+
+import bpy
+from ...node_tree.base_node import SN_ScriptingBaseNode
+
+
+class SN_CombineTextNode(bpy.types.Node, SN_ScriptingBaseNode):
+
+    bl_idname = "SN_CombineTextNode"
+    bl_label = "Combine Text"
+    bl_icon = "PLUS"
+    node_color = (0.65,0,0)
+    should_be_registered = False
+
+    def inititialize(self,context):
+        self.sockets.create_input(self,"STRING","Value")
+        self.sockets.create_input(self,"STRING","Value")
+        self.sockets.create_output(self,"STRING","Output")
+
+    def evaluate(self, socket, input_data, errors):
+        return {
+            "blocks": [
+                {
+                    "lines": [ # lines is a list of lists, where the lists represent the different lines
+                    ],
+                    "indented": [ # indented is a list of lists, where the lists represent the different lines
+                    ]
+                }
+            ],
+            "errors": []
+        }
