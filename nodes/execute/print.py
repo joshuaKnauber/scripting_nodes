05a159710ed2e107a40f21608e207af9c5afd62c
BuildTools <finn.knauber@gmail.com>
2020-07-23 15:26:10 +0200

    added print node

--- nodes/execute/print.py
+++ nodes/execute/print.py
@@ -1,23 +1,21 @@
-#SN_IfProgramNode
+#SN_PrintNode
 
 import bpy
 from ...node_tree.base_node import SN_ScriptingBaseNode
 
 
-class SN_IfProgramNode(bpy.types.Node, SN_ScriptingBaseNode):
+class SN_PrintNode(bpy.types.Node, SN_ScriptingBaseNode):
 
-    bl_idname = "SN_IfProgramNode"
-    bl_label = "If (Program)"
-    bl_icon = "CON_ACTION"
+    bl_idname = "SN_PrintNode"
+    bl_label = "Print"
+    bl_icon = "CONSOLE"
     node_color = (1,1,1)
     should_be_registered = False
 
     def inititialize(self,context):
         self.sockets.create_input(self,"EXECUTE","Execute")
-        self.sockets.create_input(self,"BOOLEAN","Value")
+        self.sockets.create_input(self,"DATA","Value")
         self.sockets.create_output(self,"EXECUTE","Execute")
-        self.sockets.create_output(self,"EXECUTE","Do")
-        self.sockets.create_output(self,"EXECUTE","Else")
 
     def evaluate(self, socket, input_data, errors):
         return {
