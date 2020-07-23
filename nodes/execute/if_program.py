a86f468b67c9c0f5c72386801ff073050d9302ea
BuildTools <finn.knauber@gmail.com>
2020-07-23 15:12:50 +0200

    added if program node

---
+++ nodes/execute/if_program.py
@@ -0,0 +1,33 @@
+#SN_IfProgramNode
+
+import bpy
+from ...node_tree.base_node import SN_ScriptingBaseNode
+
+
+class SN_IfProgramNode(bpy.types.Node, SN_ScriptingBaseNode):
+
+    bl_idname = "SN_IfProgramNode"
+    bl_label = "If (Program)"
+    bl_icon = "NONE"
+    node_color = (1,1,1)
+    should_be_registered = False
+
+    def inititialize(self,context):
+        self.sockets.create_input(self,"EXECUTE","Execute")
+        self.sockets.create_input(self,"BOOLEAN","Value")
+        self.sockets.create_output(self,"EXECUTE","Execute")
+        self.sockets.create_output(self,"EXECUTE","Do")
+        self.sockets.create_output(self,"EXECUTE","Else")
+
+    def evaluate(self, socket, input_data, errors):
+        return {
+            "blocks": [
+                {
+                    "lines": [ # lines is a list of lists, where the lists represent the different lines
+                    ],
+                    "indented": [ # indented is a list of lists, where the lists represent the different lines
+                    ]
+                }
+            ],
+            "errors": []
+        }
