3da7f553b1c7a0cd874a36c83ecd4eb4b442e8d3
BuildTools <finn.knauber@gmail.com>
2020-08-14 23:06:43 +0200

    added repeat layout

--- nodes/interface/repeat.py
+++ nodes/interface/repeat.py
@@ -1,22 +1,22 @@
-#SN_RepeatProgramNode
+#SN_RepeatLayoutNode
 
 import bpy
 from ...node_tree.base_node import SN_ScriptingBaseNode
 
 
-class SN_RepeatProgramNode(bpy.types.Node, SN_ScriptingBaseNode):
+class SN_RepeatLayoutNode(bpy.types.Node, SN_ScriptingBaseNode):
 
-    bl_idname = "SN_RepeatProgramNode"
-    bl_label = "Repeat (Program)"
+    bl_idname = "SN_RepeatLayoutNode"
+    bl_label = "Repeat (Layout)"
     bl_icon = "CON_ACTION"
-    node_color = (0.2, 0.2, 0.2)
+    node_color = (0.89,0.6,0)
     should_be_registered = False
 
     def inititialize(self,context):
-        self.sockets.create_input(self,"EXECUTE","Execute")
+        self.sockets.create_input(self,"LAYOUT","Layout")
         self.sockets.create_input(self,"INTEGER","Repetitions")
-        self.sockets.create_output(self,"EXECUTE","Execute")
-        self.sockets.create_output(self,"EXECUTE","Repeat")
+        self.sockets.create_output(self,"LAYOUT","Layout")
+        self.sockets.create_output(self,"LAYOUT","Repeat")
         self.sockets.create_output(self,"INTEGER","Step")
 
     def evaluate(self, socket, input_data, errors):
