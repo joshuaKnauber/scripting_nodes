4c43e83f0cb7e379c8753bbb2992a3554c300cff
BuildTools <finn.knauber@gmail.com>
2020-11-09 21:51:02 +0100

    changelog

--- properties/addon_preferences.py
+++ properties/addon_preferences.py
@@ -177,8 +177,11 @@ class ScriptingNodesAddonPreferences(bpy.types.AddonPreferences):
             box = col.box()
             _col = box.column(align=True)
             # _col.label(text="    • ")
+            _col.label(text="    • Fixed general bugs")
             _col.label(text="    • Added insert keyframe node")
             _col.label(text="    • Fixed issues with numbers at the start of operators")
+            _col.label(text="    • Added lights and nodes to define type")
+            _col.label(text="    • Added node to set active")
 
         elif self.main_nav == "PACKAGES":
 
