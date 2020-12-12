315f87a5cec73acadade204902867d9134158fad
= <=>
2020-12-12 11:19:05 +0100

    socket value

--- node_tree/sockets.py
+++ node_tree/sockets.py
@@ -2,18 +2,27 @@ import bpy
 
 
 
-class SN_StringSocket(bpy.types.NodeSocket):
+class ScriptingSocket:
+    pass
+
+
+
+class SN_StringSocket(bpy.types.NodeSocket, ScriptingSocket):
     bl_label = "String"
     
-    value: bpy.props.StringProperty(default="",
+    text: bpy.props.StringProperty(default="",
                                     name="Value",
                                     description="Value of this socket")
+    
+    @property
+    def value(self):
+        return self.text
 
     def draw(self, context, layout, node, text):
         if self.is_output or self.is_linked:
             layout.label(text=text)
         else:
-            layout.prop(self, "value", text=text)
+            layout.prop(self, "text", text=text)
 
     def draw_color(self, context, node):
         return (1.0, 0.4, 0.216, 0.5)
