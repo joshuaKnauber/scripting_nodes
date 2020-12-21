17b500402c1f38de9140ddf75909c631cb90d904
BuildTools <finn.knauber@gmail.com>
2020-12-21 21:55:14 +0100

    added math node

--- node_tree/sockets/text.py
+++ node_tree/sockets/text.py
@@ -7,8 +7,8 @@ from ...compiler.compiler import process_node
 class SN_StringSocket(bpy.types.NodeSocket, ScriptingSocket):
     bl_label = "String"
     sn_type = "STRING"
-    connects_to = ["SN_StringSocket","SN_DynamicDataSocket","SN_BooleanSocket","SN_FloatSocket", "SN_IntSocket",
-                   "SN_VariableSocket","SN_DynamicVariableSocket","SN_DataSocket","SN_DynamicStringSocket"]
+    connects_to = ["SN_StringSocket","SN_DynamicDataSocket","SN_BooleanSocket","SN_FloatSocket", "SN_IntegerSocket",
+                   "SN_VariableSocket","SN_DynamicVariableSocket","SN_DataSocket","SN_DynamicStringSocket","SN_NumberSocket"]
     
     default_value: bpy.props.StringProperty(default="",
                                             update=ScriptingSocket.socket_value_update,
@@ -44,8 +44,8 @@ class SN_StringSocket(bpy.types.NodeSocket, ScriptingSocket):
 
 
 class SN_DynamicStringSocket(bpy.types.NodeSocket, DynamicSocket):
-    connects_to = ["SN_StringSocket", "SN_DataSocket","SN_BooleanSocket","SN_FloatSocket","SN_IntSocket",
-                   "SN_VariableSocket"]
+    connects_to = ["SN_StringSocket", "SN_DataSocket","SN_BooleanSocket","SN_FloatSocket","SN_IntegerSocket",
+                   "SN_VariableSocket","SN_NumberSocket"]
     add_idname = "SN_StringSocket"
     
     
--- node_tree/sockets/variable.py
+++ node_tree/sockets/variable.py
@@ -5,6 +5,6 @@ from ...compiler.compiler import process_node
 
 
 class SN_DynamicVariableSocket(bpy.types.NodeSocket, DynamicSocket):
-    connects_to = ["SN_StringSocket", "SN_DataSocket","SN_BooleanSocket","SN_FloatSocket","SN_IntSocket"]
+    connects_to = ["SN_StringSocket", "SN_DataSocket","SN_BooleanSocket","SN_FloatSocket","SN_IntegerSocket","SN_NumberSocket"]
     make_variable = True
     add_idname = "SN_DataSocket"
