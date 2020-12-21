ff215dc276cff030047ec06629d99e87f6a220cb
= <=>
2020-12-21 14:36:02 +0100

    add nodes

--- interface/menu/add_menu.py
+++ interface/menu/add_menu.py
@@ -1,4 +1,5 @@
 import bpy
+from ...node_tree.node_categories import get_node_categories
         
         
         
@@ -12,16 +13,40 @@ class SN_OT_RunAddMenu(bpy.types.Operator):
         return {"FINISHED"}
 
     def invoke(self,context,event):
-        if event.shift and event.type == "LEFTMOUSE":
+        from_node = context.space_data.node_tree.nodes.active
+        if event.shift and event.type == "LEFTMOUSE" and from_node and from_node.select:
             context.scene.compatible_nodes.clear()
-            for i in range(20):
-                item = context.scene.compatible_nodes.add()
-                item.name = str(i)
-                item.category = str(i%5)
-                item.idname = "SN_BooleanNode"
+            for category in get_node_categories():
+                for node in category.items(context):
+                    item = context.scene.compatible_nodes.add()
+                    item.category = category.name
+                    item.name = "  "+node.label
+                    item.idname = node.nodetype
             
             bpy.ops.wm.call_menu(name="SN_MT_AddNodeMenu")
         return {"FINISHED"}
+    
+    
+
+class SN_OT_AddNode(bpy.types.Operator):
+    bl_idname = "sn.add_node"
+    bl_label = "Add Node"
+    bl_description = "Adds this node"
+    bl_options = {"REGISTER","UNDO","INTERNAL"}
+    
+    idname: bpy.props.StringProperty()
+
+    def execute(self, context):
+        from_node = context.space_data.node_tree.nodes.active
+        bpy.ops.node.add_node(type=self.idname)
+        node = context.space_data.node_tree.nodes.active
+        node.location = (node.location[0],node.location[1])
+        
+        return {"FINISHED"}
+    
+    def invoke(self,context,event):
+        
+        return self.execute(context)
     
 
 
@@ -39,9 +64,9 @@ class SN_MT_AddNodeMenu(bpy.types.Menu):
             categories[item.category].append(item)
             
         layout.operator_context = "INVOKE_DEFAULT"
+        layout.operator("node.add_search",text="Search...",icon="VIEWZOOM").use_transform = True
         for category in categories:
             layout.label(text=category)
             for item in categories[category]:
-                op = layout.operator("node.add_node", text=item.name)
-                op.type = item.idname
-                op.use_transform = True
+                op = layout.operator("sn.add_node", text=item.name)
+                op.idname = item.idname
