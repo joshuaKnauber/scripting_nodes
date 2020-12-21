ad757b417f6efcd6ee3d0af67aff74feeba0a85f
BuildTools <finn.knauber@gmail.com>
2020-12-21 22:22:08 +0100

    added radians degrees node

---
+++ nodes/Logic/radians_degrees.py
@@ -0,0 +1,35 @@
+import bpy
+from ...node_tree.base_node import SN_ScriptingBaseNode, SN_GenericPropertyGroup
+
+
+
+class SN_RadiansNode(bpy.types.Node, SN_ScriptingBaseNode):
+
+    bl_idname = "SN_RadiansNode"
+    bl_label = "Radians - Degrees"
+    # bl_icon = "GRAPH"
+    bl_width_default = 135
+
+    node_options = {
+        "default_color": (0.3,0.3,0.3),
+    }
+
+
+    operation: bpy.props.EnumProperty(items=[("degrees", "Radians to Degrees", "Convert Radians to Degrees"), ("radians", "Degrees to Radians", "Convert Degrees to radians")],name="Operation", update=SN_ScriptingBaseNode.update_needs_compile)
+
+
+    def on_create(self,context):
+        self.add_float_input("Input")
+        self.add_float_output("Output")
+
+
+    def draw_node(self, context, layout):
+        col = layout.column()
+        col.prop(self, "operation", expand=True)
+
+
+    def code_evaluate(self, context, touched_socket):
+
+        return {
+            "code": f"""math.{self.operation}({self.inputs[0].value})"""
+        }
