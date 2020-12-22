7cf010a29b4d9dcc2033e0718c36814aacadf549
= <=>
2020-12-22 19:31:19 +0100

    fix and script node

---
+++ nodes/Program/0_run_script.py
@@ -0,0 +1,28 @@
+import bpy
+from ...node_tree.base_node import SN_ScriptingBaseNode, SN_GenericPropertyGroup
+
+
+
+class SN_RunScriptNode(bpy.types.Node, SN_ScriptingBaseNode):
+
+    bl_idname = "SN_RunScriptNode"
+    bl_label = "Script"
+    # bl_icon = "GRAPH"
+    bl_width_default = 160
+
+    node_options = {
+        "default_color": (0.2,0.2,0.2),
+        "starts_tree": True
+    }
+
+
+    def on_create(self,context):
+        self.add_execute_output("Script")
+
+
+    def code_evaluate(self, context, touched_socket):
+        return {
+            "code": f"""
+                    {self.outputs[0].block(5)}
+                    """
+        }
--- nodes/Program/1_run_function.py
+++ nodes/Program/1_run_function.py
@@ -109,20 +109,22 @@ class SN_RunFunctionNode(bpy.types.Node, SN_ScriptingBaseNode):
             if touched_socket == self.inputs[0]:
                 return {
                     "code": f"""
-                        {self.addon_tree.sn_nodes["SN_FunctionNode"].items[self.func_name].identifier}({self.list_blocks(parameters, 0)})
-                        {self.outputs[0].block(5)}
-                        """
+                            {self.addon_tree.sn_nodes["SN_FunctionNode"].items[self.func_name].identifier}({self.list_blocks(parameters, 0)})
+                            {self.outputs[0].block(7)}
+                            """
                 }
 
             else:
                 return {
                     "code": f"""
-                        {self.addon_tree.sn_nodes["SN_FunctionNode"].items[self.func_name].identifier}({self.list_blocks(parameters, 0)})[{self.outputs.find(touched_socket.name)-1}]
-                        """
+                            {self.addon_tree.sn_nodes["SN_FunctionNode"].items[self.func_name].identifier}({self.list_blocks(parameters, 0)})[{self.outputs.find(touched_socket.name)-1}]
+                            """
                 }
 
         else:
             self.add_error("No function", "No valid function selected")
             return {
-                "code": {self.outputs[0].block(5)}
+                "code": f"""
+                        {self.outputs[0].block(6)}
+                        """
             }
