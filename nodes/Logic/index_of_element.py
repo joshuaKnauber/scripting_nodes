ffe494f745c184b3afd69962bc93a699e8762a53
BuildTools <finn.knauber@gmail.com>
2021-01-12 18:59:37 +0100

    added index element node

--- nodes/Logic/index_of_element.py
+++ nodes/Logic/index_of_element.py
@@ -3,10 +3,10 @@ from ...node_tree.base_node import SN_ScriptingBaseNode, SN_GenericPropertyGroup
 
 
 
-class SN_IsInListNode(bpy.types.Node, SN_ScriptingBaseNode):
+class SN_IndexElementNode(bpy.types.Node, SN_ScriptingBaseNode):
 
-    bl_idname = "SN_IsInListNode"
-    bl_label = "Is In List"
+    bl_idname = "SN_IndexElementNode"
+    bl_label = "Index in List"
     # bl_icon = "GRAPH"
     bl_width_default = 160
 
@@ -19,11 +19,11 @@ class SN_IsInListNode(bpy.types.Node, SN_ScriptingBaseNode):
     def on_create(self,context):
         self.add_list_input("List")
         self.add_data_input("Element")
-        self.add_boolean_output("Is In List")
+        self.add_integer_output("Index of Element")
 
 
     def code_evaluate(self, context, touched_socket):
 
         return {
-            "code": f"""{self.inputs[1].code()} in {self.inputs[0].code()}"""
+            "code": f"""{self.inputs[0].code()}.index({self.inputs[1].code()})"""
         }
