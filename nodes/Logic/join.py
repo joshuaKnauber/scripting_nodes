d4109427a1840fb42327b6fa7ef35e4c08af9256
BuildTools <finn.knauber@gmail.com>
2021-01-23 20:14:58 +0100

    added String nodes

---
+++ nodes/Logic/join.py
@@ -0,0 +1,29 @@
+import bpy
+from ...node_tree.base_node import SN_ScriptingBaseNode, SN_GenericPropertyGroup
+
+
+
+class SN_JoinListNode(bpy.types.Node, SN_ScriptingBaseNode):
+
+    bl_idname = "SN_JoinListNode"
+    bl_label = "Join String List"
+    # bl_icon = "GRAPH"
+    bl_width_default = 160
+
+
+    node_options = {
+        "default_color": (0.3,0.3,0.3),
+    }
+
+
+    def on_create(self,context):
+        self.add_list_input("List")
+        self.add_string_input("Join On")
+        self.add_string_output("String")
+
+
+    def code_evaluate(self, context, touched_socket):
+
+        return {
+            "code": f"""{self.inputs[1].code()}.join({self.inputs[0].code()})"""
+        }
