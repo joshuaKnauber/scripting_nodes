452b75ecf6a3a497896a452165859b7285ab4c09
Finn <finn.knauber@gmail.com>
2021-08-21 22:43:11 +0200

    fixed update property node with subtype change

--- nodes/Properties/property_update.py
+++ nodes/Properties/property_update.py
@@ -29,12 +29,15 @@ class SN_UpdatePropertyNode(bpy.types.Node, SN_ScriptingBaseNode):
                         if data["property"]["type"] != new_data["property"]["type"]:
                             self["copied_path"] = string_data
                             self.change_socket_type(self.outputs[1], self.prop_types[new_data["property"]["type"]])
-                        if new_data["property"]["type"] == "ENUM":
-                            if data["property"]["items"] != new_data["property"]["items"]:
-                                self["copied_path"] = string_data
+
+                        if data["property"]["subtype"] != new_data["property"]["subtype"]:
+                            self["copied_path"] = string_data
 
                         self.outputs[1].subtype = self.subtype_from_prop_subtype(new_data["property"]["type"],new_data["property"]["subtype"],new_data["property"]["size"])
                         self.outputs[1].variable_name = new_data["property"]["identifier"]
+                    if new_data["property"]["type"] == "ENUM":
+                        if data["property"]["items"] != new_data["property"]["items"]:
+                            self["copied_path"] = string_data
                     if new_data["property"]["removed"]:
                         self.copied_path = ""
 
@@ -50,6 +53,9 @@ class SN_UpdatePropertyNode(bpy.types.Node, SN_ScriptingBaseNode):
                         if data["property"]["type"] != new_data["property"]["type"]:
                             self["copied_path"] = string_data
                             self.change_socket_type(self.outputs[1], self.prop_types[new_data["property"]["type"]])
+
+                        if data["property"]["subtype"] != new_data["property"]["subtype"]:
+                            self["copied_path"] = string_data
 
                         self.outputs[1].subtype = self.subtype_from_prop_subtype(new_data["property"]["type"],new_data["property"]["subtype"],new_data["property"]["size"])
                         self.outputs[1].variable_name = new_data["property"]["identifier"]
