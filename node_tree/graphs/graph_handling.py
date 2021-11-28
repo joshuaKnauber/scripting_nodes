53c1de2d3458136c42fb83d56d01f685338c972d
Finn <finn.knauber@gmail.com>
2021-11-28 18:13:39 +0100

    append tree

--- node_tree/graphs/graph_handling.py
+++ node_tree/graphs/graph_handling.py
@@ -1,4 +1,6 @@
+from bpy_extras.io_utils import ImportHelper
 import bpy
+import os
 
 
 class SN_OT_RemoveGraph(bpy.types.Operator):
@@ -19,5 +21,60 @@ class SN_OT_RemoveGraph(bpy.types.Operator):
 
     def invoke(self, context, event):
         return context.window_manager.invoke_confirm(self, event)
+
+
+
+class SN_OT_AppendPopup(bpy.types.Operator):
+    bl_idname = "sn.append_popup"
+    bl_label = "Append Graph"
+    bl_description = "Appends this graph from the addon"
+    bl_options = {"REGISTER", "UNDO", "INTERNAL"}
+    
+    def get_graph_items(self,context):
+        items = []
+        with bpy.data.libraries.load(self.path) as (data_from, data_to):
+            for group in data_from.node_groups:
+                items.append((group,group,group))
+        if not items:
+            items = [("NONE","NONE","NONE")]
+        return items
+
+    path: bpy.props.StringProperty()
+    graphs: bpy.props.EnumProperty(name="Graph",
+                                   description="Graph to import",
+                                   items=get_graph_items)
+
+    def execute(self, context):
+        if self.graphs != "NONE":
+            bpy.ops.wm.append(filename=self.graphs, directory=self.path + "\\NodeTree\\")
+            #TODO somehow compile new tree
+
+        return {"FINISHED"}
+    
+    def draw(self,context):
+        if self.graphs == "NONE":
+            self.layout.label(text="No Graphs found in this blend file",icon="ERROR")
+        else:
+            self.layout.prop(self,"graphs",text="Graph")
+
+    def invoke(self, context, event):
+        return context.window_manager.invoke_props_dialog(self)
+    
+    
+    
+class SN_OT_AppendGraph(bpy.types.Operator, ImportHelper):
+    bl_idname = "sn.append_graph"
+    bl_label = "Append Graph"
+    bl_description = "Appends a graph from another file to this addon"
+    bl_options = {"REGISTER", "UNDO", "INTERNAL"}
+    
+    filter_glob: bpy.props.StringProperty( default='*.blend', options={'HIDDEN'} )
+
+    def execute(self, context):
+        filename, extension = os.path.splitext(self.filepath)
+        if extension == ".blend":
+            bpy.ops.sn.append_popup("INVOKE_DEFAULT",path=self.filepath)
+        return {"FINISHED"}
+
 
 
