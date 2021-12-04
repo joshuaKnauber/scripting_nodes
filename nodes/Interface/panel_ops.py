f28483a5424c94e5219588a990c781c38736a4f4
Joshua Knauber <joshua.knauber@gmail.com>
2021-12-04 18:55:35 +0100

    comments

--- nodes/Interface/panel_ops.py
+++ nodes/Interface/panel_ops.py
@@ -43,6 +43,7 @@ class {idname}(bpy.types.Panel):
 bpy.utils.register_class({idname})
 registered_panels.append({idname})
 """
+    #register panel
     try: exec(panel, globals())
     except: pass
      
@@ -62,6 +63,7 @@ class SN_OT_ActivatePanelPicker(bpy.types.Operator):
         return not context.scene.sn.picker_active
 
     def execute(self, context):
+        # possible panel locations
         space_types = ["EMPTY", "VIEW_3D", "IMAGE_EDITOR", "NODE_EDITOR",
                         "SEQUENCE_EDITOR", "CLIP_EDITOR", "DOPESHEET_EDITOR",
                         "GRAPH_EDITOR", "NLA_EDITOR", "TEXT_EDITOR", "CONSOLE",
@@ -71,14 +73,17 @@ class SN_OT_ActivatePanelPicker(bpy.types.Operator):
                         "TOOL_PROPS", "PREVIEW", "HUD", "NAVIGATION_BAR", "EXECUTE",
                         "FOOTER", "TOOL_HEADER", "XR"]
 
-        registered_panels = []
+        # register panels
+        registered_panels.clear()
         for space in space_types:
             for region in region_types:
                 register_dummy_panel(self.node_tree, self.node, space, region)
         
+        # redraw screen
         for area in context.screen.areas:
             area.tag_redraw()
 
+        # set picker active
         context.scene.sn.picker_active = True
         return {"FINISHED"}
      
@@ -98,6 +103,7 @@ class SN_OT_PickPanelLocation(bpy.types.Operator):
     context: bpy.props.StringProperty(options={"SKIP_SAVE", "HIDDEN"})
 
     def execute(self, context):
+        # find node and set properties
         if self.node_tree in bpy.data.node_groups:
             ntree = bpy.data.node_groups[self.node_tree]
             if self.node in ntree.nodes:
@@ -106,10 +112,12 @@ class SN_OT_PickPanelLocation(bpy.types.Operator):
                 node.region = self.region
                 node.context = self.context
 
+        # unregister panels
         for panel in registered_panels:
             try: bpy.utils.unregister_class(panel)
             except: pass
 
+        # reset saves and picker
         registered_panels.clear()
         context.scene.sn.picker_active = False
         return {"FINISHED"}
