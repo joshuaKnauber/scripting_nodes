c645a07220eb9858bc5bf7a7b8a5514acd893e59
= <=>
2021-12-21 19:12:11 +0100

    removed bug report button

--- interface/header/header.py
+++ interface/header/header.py
@@ -46,4 +46,3 @@ def header_append(self, context):
         col.operator("sn.force_compile", text="", icon="FILE_REFRESH")
         sub_row.operator("sn.force_unregister", text="", icon="UNLINKED")
         row.operator("wm.url_open", text="", icon_value=bpy.context.scene.sn_icons["discord"].icon_id).url = "https://discord.com/invite/NK6kyae"
-        row.operator("wm.url_open", text="", icon_value=bpy.context.scene.sn_icons["bug"].icon_id).url = "https://joshuaknauber.github.io/visual_scripting_addon_docs/visual_scripting_docs/site/bugs/"
