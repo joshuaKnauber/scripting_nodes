81d445f7617ee6b6e66b761f12f8a6deed2ce191
= <=>
2022-01-08 23:03:27 +0100

    changed pref ref

--- settings/addon_preferences.py
+++ settings/addon_preferences.py
@@ -53,4 +53,4 @@ class SN_AddonPreferences(bpy.types.AddonPreferences):
 
 
 
-# addon_prefs = context.preferences.addons[bpy.context.scene.sn.addon_tree().sn_graphs[0].short()].preferences
+# addon_prefs = bpy.context.preferences.addons[__name__.partition('.')[ 0]].preferences
