f64dde85c54fee7cb9374e92e569a5599dc4dbd7
= <=>
2022-01-12 11:15:46 +0100

    remove debug

--- interface/panels/property_ui_list.py
+++ interface/panels/property_ui_list.py
@@ -7,6 +7,5 @@ class SN_UL_PropertyList(bpy.types.UIList):
         row = layout.row()
         row.label(text="", icon=item.icon)
         row.prop(item, "name", emboss=False, text="")
-        row.label(text=item.python_name)
         if not item.property_type == "Group":
             row.operator("sn.copy_python_name", text="", icon="COPYDOWN", emboss=False).name = item.data_path
