8f00cdf899ce2eb3c1a1d7216a360ac9cf5cbce9
= <=>
2022-02-20 14:56:15 +0100

    improve free compile with timer

--- nodes/base_node.py
+++ nodes/base_node.py
@@ -359,7 +359,8 @@ class SN_ScriptingBaseNode:
         # free node
         self.on_free()
         # recompile without node
-        compile_addon()
+        if not bpy.app.timers.is_registered(compile_addon):
+            bpy.app.timers.register(compile_addon, first_interval=0.25)
 
 
     ### NODE UPDATE
