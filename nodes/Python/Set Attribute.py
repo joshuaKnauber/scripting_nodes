64609771d7c14b9e6e711e96181bcd83b3e63f85
= <joshua.knauber@gmail.com>
2022-04-07 23:22:34 +0200

    fixed set custom property node

--- nodes/Python/Set Attribute.py
+++ nodes/Python/Set Attribute.py
@@ -13,10 +13,11 @@ class SN_SetAttributeNode(bpy.types.Node, SN_ScriptingBaseNode):
         self.add_execute_input()
         self.add_data_input()
         self.add_string_input("Attribute")
+        self.add_data_input("Value")
         self.add_execute_output()
         
     def evaluate(self, context):
         self.code = f"""
-                    setattr({self.inputs[1].python_value}, {self.inputs['Attribute'].python_value})
+                    setattr({self.inputs[1].python_value}, {self.inputs['Attribute'].python_value}, {self.inputs['Value'].python_value})
                     {self.indent(self.outputs[0].python_value, 5)}
                     """
