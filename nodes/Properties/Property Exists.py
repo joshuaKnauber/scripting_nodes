bba22a854c0b34d8c5b42ae74d26679ad6086e1d
Joshua Knauber <joshua.knauber@gmail.com>
2022-04-09 14:50:47 +0200

    fixed property exists

--- nodes/Properties/Property Exists.py
+++ nodes/Properties/Property Exists.py
@@ -16,23 +16,12 @@ class SN_PropertyExistsNode(bpy.types.Node, SN_ScriptingBaseNode):
 
 
     def evaluate(self, context):
-        sections = self.inputs[0].python_sections
-        parts = []
-        path = ""
-        for section in sections:
-            if section[0] == "[" and section[-1] == "]":
-                if section[1:-1].isdigit():
-                    parts.append(f"len({path}) > {section[1:-1]}")
-                else:
-                    parts.append(f"{section[1:-1]} in {path}")
-                path += section
-            else:
-                if path:
-                    parts.append(f"""hasattr({path}, "{section}")""")
-                    path += "." + section
-                else:
-                    path += section
-        if parts:
-            self.outputs[0].python_value = f"({' and '.join(parts)})"
-        else:
-            self.outputs[0].python_value = f"False"
+        self.code_imperative = f"""
+            def property_exists(prop_path):
+                try:
+                    eval(prop_path)
+                    return True
+                except:
+                    return False
+            """
+        self.outputs[0].python_value = f"property_exists('{self.inputs[0].python_value}')"
