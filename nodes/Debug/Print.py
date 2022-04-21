a3d390a17def9d8c0fb8d32cf87a4f569639ba40
= <joshua.knauber@gmail.com>
2022-04-21 14:22:11 +0200

    added refresh on print node

--- nodes/Debug/Print.py
+++ nodes/Debug/Print.py
@@ -50,6 +50,7 @@ class SN_PrintNode(bpy.types.Node, SN_ScriptingBaseNode):
                         if '{self.node_tree.name}' in bpy.data.node_groups and '{self.name}' in bpy.data.node_groups['{self.node_tree.name}'].nodes:
                             msg = bpy.data.node_groups['{self.node_tree.name}'].nodes['{self.name}'].messages.add()
                             msg.text = str([{", ".join(values)}])[1:-1]
+                        for area in bpy.context.screen.areas: area.tag_redraw()
                     except:
                         print("Can't add print outputs to the node when the print is run in an interface!")
                     {self.indent(self.outputs[0].python_value, 5)}
