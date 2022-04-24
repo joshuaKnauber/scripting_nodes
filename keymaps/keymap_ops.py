218311f393f649b556a3d6264353cf1133fb4b42
= <joshua.knauber@gmail.com>
2022-04-24 14:06:19 +0200

    added pie menu for copied operators with same shortcut

--- keymaps/keymap_ops.py
+++ keymaps/keymap_ops.py
@@ -15,15 +15,47 @@ class SN_OT_OpenNodeDocs(bpy.types.Operator):
     
     
     
-class SN_OT_AddBlenderPropertyNode(bpy.types.Operator):
-    bl_idname = "sn.add_blend_property"
-    bl_label = "Add Blender Property Node"
-    bl_description = "Adds a blender property node and pastes the filepath if available"
+class SN_MT_AddOperatorMenu(bpy.types.Menu):
+    bl_idname = "SN_MT_AddOperatorMenu"
+    bl_label = "Add Operator Node"
+
+    def draw(self, context):
+        layout = self.layout.menu_pie()
+        layout.operator("sn.add_copied_operator_node", text="Run Operator", icon="POSE_HLT").is_button = False
+        layout.operator("sn.add_copied_operator_node", text="Button", icon="MOUSE_LMB").is_button = True
+    
+    
+class SN_OT_AddCopiedNode(bpy.types.Operator):
+    bl_idname = "sn.add_copied_node"
+    bl_label = "Add Copied Node"
+    bl_description = "Adds a node from the copied path for operators and properties"
     bl_options = {"REGISTER", "INTERNAL"}
 
     def execute(self, context):
-        bpy.ops.node.add_node("INVOKE_DEFAULT", type="SN_BlenderPropertyNode", use_transform=True)
-        node = context.space_data.node_tree.nodes.active
-        if "bpy." in context.window_manager.clipboard and not ".ops." in context.window_manager.clipboard:
+        if "bpy." in context.window_manager.clipboard and ".ops." in context.window_manager.clipboard:
+            bpy.ops.wm.call_menu_pie(name="SN_MT_AddOperatorMenu")
+        elif "bpy." in context.window_manager.clipboard and not ".ops." in context.window_manager.clipboard:
+            bpy.ops.node.add_node("INVOKE_DEFAULT", type="SN_BlenderPropertyNode", use_transform=True)
+            node = context.space_data.node_tree.nodes.active
             bpy.ops.sn.paste_data_path(node=node.name, node_tree=context.space_data.node_tree.name)
+        return {"FINISHED"}
+    
+    
+class SN_OT_AddCopiedOperatorNode(bpy.types.Operator):
+    bl_idname = "sn.add_copied_operator_node"
+    bl_label = "Add Copied Operator Node"
+    bl_description = "Adds a node from the copied path for operators"
+    bl_options = {"REGISTER", "INTERNAL"}
+    
+    is_button: bpy.props.BoolProperty(options={"SKIP_SAVE", "HIDDEN"})
+
+    def execute(self, context):
+        if "bpy." in context.window_manager.clipboard and ".ops." in context.window_manager.clipboard:
+            if self.is_button:
+                bpy.ops.node.add_node("INVOKE_DEFAULT", type="SN_ButtonNode", use_transform=True)
+            else:
+                bpy.ops.node.add_node("INVOKE_DEFAULT", type="SN_RunOperatorNode", use_transform=True)
+            node = context.space_data.node_tree.nodes.active
+            node.source_type = "BLENDER"
+            node.pasted_operator = context.window_manager.clipboard
         return {"FINISHED"}
