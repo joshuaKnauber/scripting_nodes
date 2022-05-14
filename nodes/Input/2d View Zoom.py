6e004e28201b4a960a96c9dabba7a6fce37cfcd0
Joshua Knauber <joshua.knauber@gmail.com>
2022-05-14 21:13:14 +0200

    changed ui scale

--- nodes/Input/2d View Zoom.py
+++ nodes/Input/2d View Zoom.py
@@ -15,14 +15,15 @@ class SN_2DViewZoomNode(bpy.types.Node, SN_ScriptingBaseNode):
     def evaluate(self, context):
         self.code_imperative = """
             def get_zoom_level(area):
+                ui_scale = bpy.context.preferences.system.ui_scale
                 for region in area.regions:
                     if region.type == "WINDOW":
                         probe = 1000
-                        ui_scale = bpy.context.preferences.system.ui_scale
                         x0, y0 = region.view2d.view_to_region(0, 0, clip=False)
-                        x1, y1 = region.view2d.view_to_region(probe * ui_scale, probe * ui_scale, clip=False)
-                        return math.sqrt((x1-x0)**2 + (y1-y0)**2) / probe
-                return 1
+                        x1, y1 = region.view2d.view_to_region(probe, probe, clip=False)
+                        zoom = math.sqrt((x1-x0)**2 + (y1-y0)**2) / probe
+                        return zoom * ui_scale
+                return 1 * ui_scale
         """
         self.code_import = "import math"
         self.outputs[0].python_value = f"get_zoom_level({self.inputs[0].python_value})"
