caf22cd15e61ced6da8fa1a6d1699e42052bd199
Joshua Knauber <joshua.knauber@gmail.com>
2022-09-25 15:07:03 +0200

    added keyframe and driver functions to blend data browser

--- nodes/Properties/Run Property Function.py
+++ nodes/Properties/Run Property Function.py
@@ -4,14 +4,12 @@ from .Blender_Property import segment_is_indexable, data_path_from_inputs
 from ...settings.data_properties import bpy_to_indexed_sections, bpy_to_path_sections
 
 
-
 class SN_RunPropertyFunctionNode(bpy.types.Node, SN_ScriptingBaseNode):
 
     bl_idname = "SN_RunPropertyFunctionNode"
     bl_label = "Run Property Function"
     node_color = "PROPERTY"
-    
-    
+
     def _disect_data_path(self, path):
         # remove assign part
         path = "(".join(path.split("(")[:-1])
@@ -21,8 +19,7 @@ class SN_RunPropertyFunctionNode(bpy.types.Node, SN_ScriptingBaseNode):
         # split data path in segments
         segments = bpy_to_indexed_sections(path)
         return segments
-    
-    
+
     def _is_valid_data_path(self, path):
         return path and "bpy." in path and not ".ops." in path
 
@@ -32,14 +29,15 @@ class SN_RunPropertyFunctionNode(bpy.types.Node, SN_ScriptingBaseNode):
                 # return bpy_to_indexed_sections(self.pasted_data_path)
                 return self._disect_data_path(self.pasted_data_path)
         return None
-    
-    
+
     def add_socket_from_param(self, param, callback):
         """ Adds a socket from the given parameter with the given add callback """
         sn = bpy.context.scene.sn
         socket_type = param.split(": ")[-1].split("[")[0]
         param_name = param.split(": ")[0]
         socket_name = param_name.replace("_", " ").title()
+        if socket_name[-1] == "*":
+            socket_name = socket_name[:-1]
         socket = callback(self.socket_names[socket_type], socket_name)
         socket.can_be_disabled = True
         socket.disabled = True
@@ -47,7 +45,7 @@ class SN_RunPropertyFunctionNode(bpy.types.Node, SN_ScriptingBaseNode):
             socket.disabled = False
         if socket_type == "Enum" or socket_type == "Enum Set":
             socket.items = f"[{param.split(': ')[-1].split('[')[-1]}"
-    
+
     def create_inputs_from_path(self):
         """ Creates the inputs for the given data path """
         # remove existing inputs
@@ -61,19 +59,22 @@ class SN_RunPropertyFunctionNode(bpy.types.Node, SN_ScriptingBaseNode):
                     name = segment.split("[")[0].replace("_", " ").title()
                     if '"' in segment or "'" in segment:
                         inp = self.add_string_input(name)
-                        inp["default_value"] = segment.split("[")[-1].split("]")[0][1:-1]
+                        inp["default_value"] = segment.split(
+                            "[")[-1].split("]")[0][1:-1]
                         inp.index_type = "String"
                     else:
                         inp = self.add_integer_input(name)
-                        inp["default_value"] = int(segment.split("[")[-1].split("]")[0])
+                        inp["default_value"] = int(
+                            segment.split("[")[-1].split("]")[0])
                         inp.index_type = "Integer"
                     inp.indexable = True
             # create parameter inputs
-            params = self.pasted_data_path.split("(")[-1].split(")")[0].split(", ")
+            params = self.pasted_data_path.split(
+                "(")[-1].split(")")[0].split(", ")
             for param in params:
                 if param.strip():
                     self.add_socket_from_param(param, self._add_input)
-                    
+
     def create_outputs_from_path(self):
         # remove existing outputs
         for _ in range(len(self.outputs)-1):
@@ -83,13 +84,12 @@ class SN_RunPropertyFunctionNode(bpy.types.Node, SN_ScriptingBaseNode):
             params = self.pasted_data_path.split(" = ")[-1].split(", ")
             for param in params:
                 self.add_socket_from_param(param, self._add_output)
-        
-        
+
     def get_pasted_prop_name(self, offset=-1):
         if self.pasted_data_path:
             return self.pasted_data_path.split(".")[offset].split("(")[0].replace("_", " ").title()
         return "Property Function"
-    
+
     def on_prop_change(self, context):
         self.disable_evaluation = True
         name = self.get_pasted_prop_name()
@@ -99,26 +99,23 @@ class SN_RunPropertyFunctionNode(bpy.types.Node, SN_ScriptingBaseNode):
         self.create_outputs_from_path()
         self.disable_evaluation = False
         self._evaluate(context)
-        
+
     pasted_data_path: bpy.props.StringProperty(name="Pasted Path",
-                                description="The full data path to the property",
-                                update=on_prop_change)
-    
+                                               description="The full data path to the property",
+                                               update=on_prop_change)
 
     def on_create(self, context):
         self.add_execute_input()
         self.add_execute_output()
-        
-        
+
     def update_require_execute(self, context):
         self.inputs[0].set_hide(not self.require_execute)
         self.outputs[0].set_hide(not self.require_execute)
         self._evaluate(context)
-        
-    require_execute: bpy.props.BoolProperty(name="Require Execute", default=True,
-                                        description="Removes the execute inputs and only gives you the return value",
-                                        update=update_require_execute)
 
+    require_execute: bpy.props.BoolProperty(name="Require Execute", default=True,
+                                            description="Removes the execute inputs and only gives you the return value",
+                                            update=update_require_execute)
 
     def evaluate(self, context):
         data = self.get_data()
@@ -127,15 +124,21 @@ class SN_RunPropertyFunctionNode(bpy.types.Node, SN_ScriptingBaseNode):
             function = data_path_from_inputs(inps, data) + "("
 
             # add function parameters
-            inp_params = list(filter(lambda inp: inp.strip(), self.pasted_data_path.split("(")[-1].split(")")[0].split(", ")))
+            inp_params = list(filter(lambda inp: inp.strip(), self.pasted_data_path.split(
+                "(")[-1].split(")")[0].split(", ")))
             for i, param in enumerate(inp_params):
+                param = param.split(": ")[0]
                 param_inp = self.inputs[len(self.inputs) - len(inp_params)+i]
                 if not param_inp.disabled:
-                    function += f"{param.split(': ')[0]}={param_inp.python_value}, "
+                    if param[-1] == "*":
+                        function += f"{param_inp.python_value}, "
+                    else:
+                        function += f"{param.split(': ')[0]}={param_inp.python_value}, "
             function += ")"
-            
+
             # add output parameters
-            out_params = list(filter(lambda inp: inp.strip(), self.pasted_data_path.split(" = ")[-1].split(", ")))
+            out_params = list(filter(lambda inp: inp.strip(
+            ), self.pasted_data_path.split(" = ")[-1].split(", ")))
             if self.require_execute:
                 results = ""
                 if " = " in self.pasted_data_path:
@@ -143,12 +146,13 @@ class SN_RunPropertyFunctionNode(bpy.types.Node, SN_ScriptingBaseNode):
                         name = param.split(": ")[0] + f"_{self.static_uid}"
                         results += f"{name}, "
                         self.outputs[i+1].python_value = name
-                if results: results = results[:-2] + " = "
+                if results:
+                    results = results[:-2] + " = "
                 # code
                 self.code = f"""
                             {results}{function}
                             {self.indent(self.outputs[0].python_value, 7)}
-                            """           
+                            """
             # no execute
             else:
                 if len(self.outputs) == 2:
@@ -157,21 +161,21 @@ class SN_RunPropertyFunctionNode(bpy.types.Node, SN_ScriptingBaseNode):
                     for i, out in enumerate(self.outputs):
                         if not i == 0:
                             out.python_value = f"{function}[{i-1}]"
-        
 
     def draw_node(self, context, layout):
         row = layout.row(align=True)
         row.scale_y = 1.2
-        op = row.operator("sn.paste_data_path", text=self.get_pasted_prop_name() if self.pasted_data_path else "Paste Function", icon="PASTEDOWN")
+        op = row.operator("sn.paste_data_path", text=self.get_pasted_prop_name(
+        ) if self.pasted_data_path else "Paste Function", icon="PASTEDOWN")
         op.node = self.name
         op.node_tree = self.node_tree.name
         if self.pasted_data_path:
             op = row.operator("sn.reset_data_path", text="", icon="LOOP_BACK")
             op.node = self.name
             op.node_tree = self.node_tree.name
-            
+
             if len(self.outputs) > 1:
                 layout.prop(self, "require_execute")
-            
+
     def draw_node_panel(self, context, layout):
         layout.prop(self, "pasted_data_path", text="Function Path")
--- settings/data_properties.py
+++ settings/data_properties.py
@@ -2,28 +2,36 @@ import bpy
 from ..addon.properties.settings.settings import property_icons
 
 
-
 def is_valid_attribute(attr):
-    ignore_attributes = ["rna_type", "original", "bl_rna", "evaluated_depsgraph_get"]
+    ignore_attributes = ["rna_type", "original",
+                         "bl_rna", "evaluated_depsgraph_get"]
     return not attr in ignore_attributes and not attr[0] == "_"
 
 
 filter_items = [("Pointer", "Pointer", "Pointer", property_icons["Property"], 1),
-    ("Collection", "Collection", "Collection", property_icons["Collection"], 2),
-    ("List", "List", "List", property_icons["List"], 4),
-    ("String", "String/Enum", "Strings and Enums", property_icons["String"], 8),
-    ("Enum Set", "Enum Set", "Enum Set", property_icons["Enum Set"], 32),
-    ("Boolean", "Boolean", "Boolean", property_icons["Boolean"], 64),
-    ("Boolean Vector", "Boolean Vector", "Boolean Vector", property_icons["Boolean"], 128),
-    ("Integer", "Integer", "Integer", property_icons["Integer"], 256),
-    ("Integer Vector", "Integer Vector", "Integer Vector", property_icons["Integer"], 512),
-    ("Float", "Float", "Float", property_icons["Float"], 1024),
-    ("Float Vector", "Float Vector", "Float Vector", property_icons["Float"], 2048),
-    ("Function", "Function", "Function", property_icons["Function"], 4096) ]
+                ("Collection", "Collection", "Collection",
+                 property_icons["Collection"], 2),
+                ("List", "List", "List", property_icons["List"], 4),
+                ("String", "String/Enum", "Strings and Enums",
+                 property_icons["String"], 8),
+                ("Enum Set", "Enum Set", "Enum Set",
+                 property_icons["Enum Set"], 32),
+                ("Boolean", "Boolean", "Boolean",
+                 property_icons["Boolean"], 64),
+                ("Boolean Vector", "Boolean Vector",
+                 "Boolean Vector", property_icons["Boolean"], 128),
+                ("Integer", "Integer", "Integer",
+                 property_icons["Integer"], 256),
+                ("Integer Vector", "Integer Vector",
+                 "Integer Vector", property_icons["Integer"], 512),
+                ("Float", "Float", "Float", property_icons["Float"], 1024),
+                ("Float Vector", "Float Vector",
+                 "Float Vector", property_icons["Float"], 2048),
+                ("Function", "Function", "Function", property_icons["Function"], 4096)]
 
 
-filter_defaults = {"Pointer","Collection","List","String","Enum Set","Boolean","Boolean Vector",
-    "Integer","Integer Vector","Float","Float Vector","Function"}
+filter_defaults = {"Pointer", "Collection", "List", "String", "Enum Set", "Boolean", "Boolean Vector",
+                   "Integer", "Integer Vector", "Float", "Float Vector", "Function"}
 
 
 def is_iterable(data):
@@ -42,11 +50,13 @@ def get_data_items(path, data):
     data_items = {}
 
     # get attributes
-    data_dict = validate_data_dict(data) if type(data) == dict else data_to_dict(data)
+    data_dict = validate_data_dict(data) if type(
+        data) == dict else data_to_dict(data)
     data_dict = add_additional_data(data_dict, path)
     for key in data_dict.keys():
         item = get_data_item(data, data_dict[key], path, key)
-        if item: data_items[key] = item
+        if item:
+            data_items[key] = item
 
     # get items for iterable
     if is_iterable(data) and not type(data) == dict:
@@ -55,7 +65,8 @@ def get_data_items(path, data):
             for key in data.keys():
                 if hasattr(data[key], "bl_rna"):
                     item = get_data_item(data, data[key], path, f"'{key}'")
-                    if item: data_items[f"'{key}'"] = item
+                    if item:
+                        data_items[f"'{key}'"] = item
         # get indexed items
         else:
             max_items = 21
@@ -67,10 +78,13 @@ def get_data_items(path, data):
                         data_items[f"{i}"]["clamped"] = True
 
     # sort items
-    sorted_keys = sorted(data_items.keys(), key=lambda s: data_items[s]["type"])
-    sorted_keys = sorted(sorted_keys, key=lambda s: data_items[s]["has_properties"], reverse=True)
+    sorted_keys = sorted(
+        data_items.keys(), key=lambda s: data_items[s]["type"])
+    sorted_keys = sorted(
+        sorted_keys, key=lambda s: data_items[s]["has_properties"], reverse=True)
     sorted_items = {}
-    for key in sorted_keys: sorted_items[key] = data_items[key]
+    for key in sorted_keys:
+        sorted_items[key] = data_items[key]
     return sorted_items
 
 
@@ -88,17 +102,22 @@ def validate_data_dict(data):
 def data_to_dict(data):
     """ Converts the given data to a dictionary with keys for the path section and the data """
     data_dict = {}
-    for attribute in dir(data):
+    attributes = dir(data)
+    if hasattr(data, "keyframe_insert"):
+        attributes += dir(bpy.types.Struct)
+        attributes.sort()
+    for attribute in attributes:
         if is_valid_attribute(attribute):
             data_dict[attribute] = getattr(data, attribute)
     return data_dict
-        
-        
+
+
 def get_data_item(parent_data, data, path, attribute):
     """ Returns a data object for the given data its path and the datas attribute """
     try:
         data
-        has_properties = hasattr(data, "bl_rna") or "bpy_prop_collection" in str(type(data))
+        has_properties = hasattr(
+            data, "bl_rna") or "bpy_prop_collection" in str(type(data))
         if (attribute[0] == "'" and attribute[-1] == "'") or attribute.isdigit():
             new_path = f"{path}[{attribute}]"
             has_properties = True
@@ -119,8 +138,10 @@ def get_data_item(parent_data, data, path, attribute):
             "clamped": False,
         }
         if data_item["type"] == "Function":
-            data_item["path"] += get_function_parameters(parent_data, attribute)
-            data_item["required"] += get_required_function_parameters(parent_data, attribute)
+            data_item["path"] += get_function_parameters(
+                parent_data, attribute)
+            data_item["required"] += get_required_function_parameters(
+                parent_data, attribute)
         return data_item
     except:
         return None
@@ -139,28 +160,89 @@ def get_data_name(data, attribute):
 def get_item_type(data):
     """ Returns the item type for the given data """
     item_type = str(type(data))
-    if "bpy_prop_collection" in item_type: item_type = "Collection"
-    elif "bpy_types" in item_type: item_type = "Pointer"
-    elif "bpy.types" in item_type: item_type = "Pointer"
-    elif hasattr(type(data), "bl_rna"): item_type = "Pointer"
-    elif "None" in item_type: item_type = "Pointer"
-    elif "bpy_func" in item_type: item_type = "Function"
-    elif "set" in item_type: item_type = "Enum Set"
-    elif "str" in item_type: item_type = "String"
-    elif "bool" in item_type: item_type = "Boolean"
-    elif "float" in item_type: item_type = "Float"
-    elif "int" in item_type: item_type = "Integer"
-    elif "Color" in item_type: item_type = "Float Vector"
-    elif "Euler" in item_type: item_type = "Float Vector"
-    elif "Quaternion" in item_type: item_type = "Float Vector"
-    elif "Matrix" in item_type: item_type = "List"
-    elif "list" in item_type: item_type = "List"
+    if "bpy_prop_collection" in item_type:
+        item_type = "Collection"
+    elif "bpy_types" in item_type:
+        item_type = "Pointer"
+    elif "bpy.types" in item_type:
+        item_type = "Pointer"
+    elif hasattr(type(data), "bl_rna"):
+        item_type = "Pointer"
+    elif "None" in item_type:
+        item_type = "Pointer"
+    elif "bpy_func" in item_type or "builtin_function_or_method" in item_type:
+        item_type = "Function"
+    elif "set" in item_type:
+        item_type = "Enum Set"
+    elif "str" in item_type:
+        item_type = "String"
+    elif "bool" in item_type:
+        item_type = "Boolean"
+    elif "float" in item_type:
+        item_type = "Float"
+    elif "int" in item_type:
+        item_type = "Integer"
+    elif "Color" in item_type:
+        item_type = "Float Vector"
+    elif "Euler" in item_type:
+        item_type = "Float Vector"
+    elif "Quaternion" in item_type:
+        item_type = "Float Vector"
+    elif "Matrix" in item_type:
+        item_type = "List"
+    elif "list" in item_type:
+        item_type = "List"
     elif ("tuple" in item_type or "Vector" in item_type or "bpy_prop_array" in item_type) and len(data):
-        if "float" in str(type(data[0])): item_type = "Float Vector"
-        elif "int" in str(type(data[0])): item_type = "Integer Vector"
-        elif "bool" in str(type(data[0])): item_type = "Boolean Vector"
-        else: item_type = "List"
+        if "float" in str(type(data[0])):
+            item_type = "Float Vector"
+        elif "int" in str(type(data[0])):
+            item_type = "Integer Vector"
+        elif "bool" in str(type(data[0])):
+            item_type = "Boolean Vector"
+        else:
+            item_type = "List"
     return item_type
+
+
+def get_special_function_params(attribute):
+    """ Returns the special parameters for the given function """
+    if attribute == "keyframe_insert":
+        return "data_path: String, index: Integer, frame: Integer, group: String, options: Enum Set['INSERTKEY_NEEDED','INSERTKEY_VISUAL','INSERTKEY_XYZ_TO_RGB','INSERTKEY_REPLACE','INSERTKEY_AVAILABLE','INSERTKEY_CYCLE_AWARE'], "
+    elif attribute == "keyframe_delete":
+        return "data_path: String, index: Integer, frame: Integer, group: String, "
+    elif attribute == "driver_add":
+        return "path*: String, index*: Integer, "
+    elif attribute == "driver_remove":
+        return "path*: String, index*: Integer, "
+    return ""
+
+
+def get_special_function_outputs(attribute):
+    """ Returns the special outputs for the given function """
+    if attribute == "as_pointer":
+        return "adress: Integer, "
+    elif attribute == "keyframe_insert":
+        return "success: Boolean, "
+    elif attribute == "keyframe_delete":
+        return "success: Boolean, "
+    elif attribute == "driver_add":
+        return "fcurve: Data, "
+    elif attribute == "driver_remove":
+        return "success: Boolean, "
+    return ""
+
+
+def get_special_required_parameters(attribute):
+    """ Returns the special required parameters for the given function """
+    if attribute == "keyframe_insert":
+        return "data_path"
+    elif attribute == "keyframe_delete":
+        return "data_path"
+    elif attribute == "driver_add":
+        return "path*"
+    elif attribute == "driver_remove":
+        return "path*"
+    return ""
 
 
 def get_function_parameters(parent_data, name):
@@ -171,21 +253,32 @@ def get_function_parameters(parent_data, name):
         if name in parent_data.bl_rna.functions:
             for param in parent_data.bl_rna.functions[name].parameters:
                 param_type = param.type.title()
-                if param_type == "Str": param_type = "String"
-                elif param_type == "Int": param_type = "Integer"
-                if getattr(param, "is_array", False): param_type += " Vector"
+                if param_type == "Str":
+                    param_type = "String"
+                elif param_type == "Int":
+                    param_type = "Integer"
+                if getattr(param, "is_array", False):
+                    param_type += " Vector"
                 if param_type == "Enum":
-                    if param.is_enum_flag: param_type = "Enum Set"
-                    items = ",".join(list(map(lambda item: f"'{item.identifier}'", param.enum_items_static)))
+                    if param.is_enum_flag:
+                        param_type = "Enum Set"
+                    items = ",".join(
+                        list(map(lambda item: f"'{item.identifier}'", param.enum_items_static)))
                     param_type += f"[{items}]"
                 if param.is_output:
                     outputs += f"{param.identifier}: {param_type}, "
                 else:
                     params += f"{param.identifier}: {param_type}, "
-    if params: params = params[:-2]
-    if outputs: outputs = outputs[:-2]
+        else:
+            params = get_special_function_params(name)
+            outputs = get_special_function_outputs(name)
+    if params:
+        params = params[:-2]
+    if outputs:
+        outputs = outputs[:-2]
     params = f"({params})"
-    if outputs: params += f" = {outputs}"
+    if outputs:
+        params += f" = {outputs}"
     return params
 
 
@@ -197,6 +290,8 @@ def get_required_function_parameters(parent_data, name):
             for param in parent_data.bl_rna.functions[name].parameters:
                 if param.is_required:
                     required += param.identifier if not required else f";{param.identifier}"
+        else:
+            required = get_special_required_parameters(name)
     return required
 
 
@@ -220,7 +315,7 @@ def item_from_path(data, path):
 def bpy_to_path_sections(path, keep_brackets=True):
     """ Takes a blender python data path and converts it to json path sections """
     if len(path) >= 4 and path[:4] == "bpy.":
-        path = path[4:]    
+        path = path[4:]
 
     sections = []
     curr_section = ""
@@ -248,7 +343,8 @@ def bpy_to_path_sections(path, keep_brackets=True):
                     curr_section += "["
             elif char == "]":
                 bracket_level -= 1
-                if keep_brackets or bracket_level > 0: curr_section += "]"
+                if keep_brackets or bracket_level > 0:
+                    curr_section += "]"
             else:
                 curr_section += char
     sections.append(curr_section)
@@ -265,8 +361,9 @@ def bpy_to_indexed_sections(path):
             combined[-1] += section
         else:
             combined.append(section)
-            
-    if not "bpy." in path: combined = combined[1:]
+
+    if not "bpy." in path:
+        combined = combined[1:]
     return combined
 
 
