02a171f9cad7cc7439d2e954795b820fd613947e
Joshua Knauber <joshua.knauber@gmail.com>
2022-10-08 16:22:42 +0200

    feat: encode byte string node

---
+++ nodes/Text/Encode String.py
@@ -0,0 +1,17 @@
+import bpy
+from ..base_node import SN_ScriptingBaseNode
+
+
+
+class SN_EncodeStringNode(bpy.types.Node, SN_ScriptingBaseNode):
+
+    bl_idname = "SN_EncodeStringNode"
+    bl_label = "Encode Byte String"
+    node_color = "STRING"
+
+    def on_create(self, context):
+        self.add_string_input("String")
+        self.add_string_output("Bytes")
+
+    def evaluate(self, context):
+        self.outputs[0].python_value = f"{self.inputs['String'].python_value}.encode() "
