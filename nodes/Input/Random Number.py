64849ec5d91a60b0abac16749c3a4c1d7249b1ce
Joshua Knauber <joshua.knauber@gmail.com>
2023-01-29 18:52:09 +0100

    fixed seed on random number node

--- nodes/Input/Random Number.py
+++ nodes/Input/Random Number.py
@@ -29,25 +29,19 @@ class SN_RandomNumberNode(SN_ScriptingBaseNode, bpy.types.Node):
 
     def evaluate(self, context):
         self.code_import = "import random"
-        if "Seed" in self.inputs and not self.inputs["Seed"].disabled:
-            self.code_imperative = """
-            def random_integer(min, max, seed):
-                random.seed(seed)
-                return random.randint(int(min), int(max))
+        self.code_imperative = """
+        def random_integer(min, max, seed):
+            random.seed(seed)
+            return random.randint(int(min), int(max))
 
-            def random_float(min, max, seed):
-                random.seed(seed)
-                return random.uniform(min, max)
-            """
-            if self.number_type == "Integer":
-                self.outputs[0].python_value = f"random_integer({self.inputs['Minimum'].python_value}, {self.inputs['Maximum'].python_value}, {self.inputs['Seed'].python_value})"
-            else:
-                self.outputs[0].python_value = f"random_float({self.inputs['Minimum'].python_value}, {self.inputs['Maximum'].python_value}, {self.inputs['Seed'].python_value})"
+        def random_float(min, max, seed):
+            random.seed(seed)
+            return random.uniform(min, max)
+        """
+        if self.number_type == "Integer":
+            self.outputs[0].python_value = f"random_integer({self.inputs['Minimum'].python_value}, {self.inputs['Maximum'].python_value}, {'None' if self.inputs['Seed'].disabled else self.inputs['Seed'].python_value})"
         else:
-            if self.number_type == "Integer":
-                self.outputs[0].python_value = f"random.randint(int({self.inputs['Minimum'].python_value}), int({self.inputs['Maximum'].python_value}))"
-            else:
-                self.outputs[0].python_value = f"random.uniform({self.inputs['Minimum'].python_value}, {self.inputs['Maximum'].python_value})"
+            self.outputs[0].python_value = f"random_float({self.inputs['Minimum'].python_value}, {self.inputs['Maximum'].python_value}, {'None' if self.inputs['Seed'].disabled else self.inputs['Seed'].python_value})"
 
     def draw_node(self, context, layout):
         layout.prop(self, "number_type")
