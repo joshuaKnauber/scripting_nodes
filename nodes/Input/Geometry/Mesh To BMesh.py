c1bb8fff28d333fbedc735de5ecd2fdab4cd1fc2
Joshua Knauber <joshua.knauber@gmail.com>
2023-01-29 19:26:39 +0100

    added evaluated depsgraph to mesh to bmesh node

--- nodes/Input/Geometry/Mesh To BMesh.py
+++ nodes/Input/Geometry/Mesh To BMesh.py
@@ -12,6 +12,9 @@ class SN_ObjectToBMeshNode(SN_ScriptingBaseNode, bpy.types.Node):
     def on_create(self, context):
         self.add_execute_input()
         self.add_property_input("Object")
+        self.add_boolean_input("Use Edit Mode").default_value = True
+        self.add_boolean_input("Use Evaluated Mesh")
+        self.add_boolean_input("Use Object Transforms").default_value = True
         self.add_execute_output()
         self.add_property_output("BMesh")
         
@@ -21,10 +24,16 @@ class SN_ObjectToBMeshNode(SN_ScriptingBaseNode, bpy.types.Node):
         self.code = f"""
             bm_{self.static_uid} = bmesh.new()
             if {self.inputs["Object"].python_value}:
-                if {self.inputs["Object"].python_value}.mode == 'EDIT':
+                if {self.inputs["Object"].python_value}.mode == 'EDIT' and {self.inputs["Use Edit Mode"].python_value}:
                     bm_{self.static_uid} = bmesh.from_edit_mesh({self.inputs["Object"].python_value}.data)
                 else:
-                    bm_{self.static_uid}.from_mesh({self.inputs["Object"].python_value}.data)
+                    if {self.inputs["Use Evaluated Mesh"].python_value}:
+                        dg = bpy.context.evaluated_depsgraph_get()
+                        bm_{self.static_uid}.from_mesh({self.inputs["Object"].python_value}.evaluated_get(dg).to_mesh())
+                    else:
+                        bm_{self.static_uid}.from_mesh({self.inputs["Object"].python_value}.data)
+            if {self.inputs["Use Object Transforms"].python_value}:
+                bm_{self.static_uid}.transform({self.inputs["Object"].python_value}.matrix_world)
             bm_{self.static_uid}.verts.ensure_lookup_table()
             bm_{self.static_uid}.faces.ensure_lookup_table()
             bm_{self.static_uid}.edges.ensure_lookup_table()
