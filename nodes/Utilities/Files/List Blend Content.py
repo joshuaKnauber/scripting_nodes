c1a534ec5a3c104529c3e0f1a7ea1d3b7a6147af
Joshua Knauber <joshua.knauber@gmail.com>
2023-11-14 18:38:00 +0100

    typo

--- nodes/Utilities/Files/List Blend Content.py
+++ nodes/Utilities/Files/List Blend Content.py
@@ -2,9 +2,7 @@ import bpy
 from ...base_node import SN_ScriptingBaseNode
 
 
-
 class SN_ListBlendContentNode(SN_ScriptingBaseNode, bpy.types.Node):
-
     bl_idname = "SN_ListBlendContentNode"
     bl_label = "List Blend File Content"
 
@@ -13,22 +11,60 @@ class SN_ListBlendContentNode(SN_ScriptingBaseNode, bpy.types.Node):
         self.add_list_output("Names")
 
     def get_append_types(self, context):
-        items = ["actions", "armatures", "bruhes", "cache_files", "cameras", "collections", "curves",
-                "images", "fonts", "grease_pencils", "lattices", "libraries", "lightprobes", "lights",
-                "linestyles", "masks", "materials", "meshes", "metaballs", "movieclips", "node_groups",
-                "objects", "paint_curves", "palettes", "particles", "pointclouds", "scenes",  "screens",
-                "shape_keys", "sounds", "speakers", "texts", "textures", "volumes", "workspaces", "worlds"]
+        items = [
+            "actions",
+            "armatures",
+            "brushes",
+            "cache_files",
+            "cameras",
+            "collections",
+            "curves",
+            "images",
+            "fonts",
+            "grease_pencils",
+            "lattices",
+            "libraries",
+            "lightprobes",
+            "lights",
+            "linestyles",
+            "masks",
+            "materials",
+            "meshes",
+            "metaballs",
+            "movieclips",
+            "node_groups",
+            "objects",
+            "paint_curves",
+            "palettes",
+            "particles",
+            "pointclouds",
+            "scenes",
+            "screens",
+            "shape_keys",
+            "sounds",
+            "speakers",
+            "texts",
+            "textures",
+            "volumes",
+            "workspaces",
+            "worlds",
+        ]
 
         tuple_items = []
         for item in items:
             tuple_items.append((item, item.replace("_", " ").title(), item))
         return tuple_items
 
-    append_type: bpy.props.EnumProperty(items=get_append_types, name="Type", description="Type of the Append object", update=SN_ScriptingBaseNode._evaluate)
+    append_type: bpy.props.EnumProperty(
+        items=get_append_types,
+        name="Type",
+        description="Type of the Append object",
+        update=SN_ScriptingBaseNode._evaluate,
+    )
 
     def draw_node(self, context, layout):
         layout.prop(self, "append_type")
-        
+
     def evaluate(self, context):
         self.code_imperative = """
         def get_blend_contents(path, data_type):
@@ -38,4 +74,8 @@ class SN_ListBlendContentNode(SN_ScriptingBaseNode, bpy.types.Node):
             return []
         """
         self.code_import = "import os"
-        self.outputs[0].python_value = f"get_blend_contents({self.inputs[0].python_value}, '{self.append_type}')"
+        self.outputs[
+            0
+        ].python_value = (
+            f"get_blend_contents({self.inputs[0].python_value}, '{self.append_type}')"
+        )
