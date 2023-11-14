a430d2b9ae64cd4516022fee3e7c6f309149941c
Joshua Knauber <joshua.knauber@gmail.com>
2023-11-14 18:12:47 +0100

    fix warnings

--- __init__.py
+++ __init__.py
@@ -10,21 +10,19 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
- 
 
 
 bl_info = {
-    "name" : "Serpens",
-    "author" : "Joshua Knauber, Finn Knauber", 
-    "description" : "Adds a node editor for building addons with nodes",
-    "blender" : (3, 0, 0),
-    "version" : (3, 2, 0),
-    "location" : "Editors -> Visual Scripting Editor",
-    "doc_url": "https://joshuaknauber.notion.site/Serpens-Documentation-d44c98df6af64d7c9a7925020af11233", 
-    "tracker_url": "https://discord.com/invite/NK6kyae", 
-    "category" : "Node" 
-} 
-
+    "name": "Serpens",
+    "author": "Joshua Knauber, Finn Knauber",
+    "description": "Adds a node editor for building addons with nodes",
+    "blender": (3, 0, 0),
+    "version": (3, 2, 0),
+    "location": "Editors -> Visual Scripting Editor",
+    "doc_url": "https://joshuaknauber.notion.site/Serpens-Documentation-d44c98df6af64d7c9a7925020af11233",
+    "tracker_url": "https://discord.com/invite/NK6kyae",
+    "category": "Node",
+}
 
 
 import bpy
@@ -34,8 +32,17 @@ import atexit
 import os
 
 from .keymaps.keymap import register_keymaps, unregister_keymaps
-from .node_tree.node_categories import draw_node_menu, register_node_menus, unregister_node_menus
-from .interface.header.header import footer_status, header_prepend, header_append, node_info_append
+from .node_tree.node_categories import (
+    draw_node_menu,
+    register_node_menus,
+    unregister_node_menus,
+)
+from .interface.header.header import (
+    footer_status,
+    header_prepend,
+    header_append,
+    node_info_append,
+)
 from .interface.panels.warnings import append_warning
 from .interface.menus.rightclick import serpens_right_click
 from .msgbus import subscribe_to_name_change, unsubscribe_from_name_change
@@ -45,24 +52,24 @@ from .settings.addon_properties import SN_AddonProperties
 from . import handlers
 
 from . import auto_load
-auto_load.init()
 
+auto_load.init()
 
 
 def register_icons():
     bpy.types.Scene.sn_icons = bpy.utils.previews.new()
-    icons_dir = os.path.join( os.path.dirname( __file__ ), "assets", "icons")
+    icons_dir = os.path.join(os.path.dirname(__file__), "assets", "icons")
 
     icons = ["discord", "serpens"]
 
     for icon in icons:
-        bpy.types.Scene.sn_icons.load(icon, os.path.join( icons_dir, icon + ".png" ), 'IMAGE')
-
+        bpy.types.Scene.sn_icons.load(
+            icon, os.path.join(icons_dir, icon + ".png"), "IMAGE"
+        )
 
 
 def unregister_icons():
-    bpy.utils.previews.remove( bpy.types.Scene.sn_icons )
-
+    bpy.utils.previews.remove(bpy.types.Scene.sn_icons)
 
 
 def register():
@@ -70,7 +77,9 @@ def register():
     auto_load.register()
 
     # addon properties
-    bpy.types.Scene.sn = bpy.props.PointerProperty(type=SN_AddonProperties, name="Serpens Properties")
+    bpy.types.Scene.sn = bpy.props.PointerProperty(
+        type=SN_AddonProperties, name="Serpens Properties"
+    )
 
     # register the keymaps
     register_keymaps()
@@ -87,11 +96,10 @@ def register():
     bpy.types.NODE_MT_editor_menus.append(header_prepend)
     bpy.types.NODE_PT_active_node_generic.append(node_info_append)
     bpy.types.STATUSBAR_HT_header.append(footer_status)
-    
+
     # add no edit warnings
-    bpy.types.NODE_PT_node_tree_interface_inputs.append(append_warning)
-    bpy.types.NODE_PT_node_tree_interface_outputs.append(append_warning)
-    
+    bpy.types.NODE_PT_node_tree_interface.append(append_warning)
+
     # add name change update
     subscribe_to_name_change()
 
@@ -102,7 +110,7 @@ def register():
     bpy.app.handlers.undo_post.append(handlers.undo_post)
     bpy.app.handlers.save_pre.append(handlers.save_pre)
     atexit.register(handlers.unload_handler)
-    
+
     # add right click menu
     bpy.types.WM_MT_button_context.append(serpens_right_click)
 
@@ -115,8 +123,7 @@ def unregister():
     bpy.types.STATUSBAR_HT_header.remove(footer_status)
 
     # remove no edit warnings
-    bpy.types.NODE_PT_node_tree_interface_inputs.remove(append_warning)
-    bpy.types.NODE_PT_node_tree_interface_outputs.remove(append_warning)
+    bpy.types.NODE_PT_node_tree_interface.remove(append_warning)
 
     # addon properties
     del bpy.types.Scene.sn
@@ -138,10 +145,10 @@ def unregister():
     bpy.app.handlers.undo_post.remove(handlers.undo_post)
     bpy.app.handlers.save_pre.remove(handlers.save_pre)
     atexit.unregister(handlers.unload_handler)
-    
+
     # remove name change msgbus
     unsubscribe_from_name_change()
-    
+
     # remove right click menu
     if hasattr(bpy.types, "WM_MT_button_context"):
         bpy.types.WM_MT_button_context.remove(serpens_right_click)
