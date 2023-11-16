f785532c91053088ea21f720897892b1c5b8b5cf
Joshua Knauber <joshua.knauber@gmail.com>
2023-11-16 16:11:13 +0100

    fix

--- nodes/Interface/Sublayouts/Layout Column.py
+++ nodes/Interface/Sublayouts/Layout Column.py
@@ -2,14 +2,12 @@ import bpy
 from ...base_node import SN_ScriptingBaseNode
 
 
-
 class SN_LayoutColumnNodeNew(SN_ScriptingBaseNode, bpy.types.Node):
-
     bl_idname = "SN_LayoutColumnNodeNew"
     bl_label = "Column"
     bl_width_default = 200
     node_color = "INTERFACE"
-    
+
     def layout_type(self, socket):
         if socket.name == "Column":
             return f"col_{self.static_uid}"
@@ -27,7 +25,9 @@ class SN_LayoutColumnNodeNew(SN_ScriptingBaseNode, bpy.types.Node):
         self.add_boolean_input("Use Invoke")["default_value"] = True
         self.add_float_input("Scale X")["default_value"] = 1
         self.add_float_input("Scale Y")["default_value"] = 1
-        self.add_enum_input("Alignment")["items"] = str(["Expand", "Left", "Center", "Right"])
+        self.add_enum_input("Alignment")["items"] = str(
+            ["Expand", "Left", "Center", "Right"]
+        )
         self.add_dynamic_interface_output("Column")
 
     def evaluate(self, context):
@@ -41,6 +41,6 @@ class SN_LayoutColumnNodeNew(SN_ScriptingBaseNode, bpy.types.Node):
                     col_{self.static_uid}.scale_x = {self.inputs["Scale X"].python_value}
                     col_{self.static_uid}.scale_y = {self.inputs["Scale Y"].python_value}
                     col_{self.static_uid}.alignment = {self.inputs["Alignment"].python_value}.upper()
-                    if not {self.inputs['Use Invoke'].python_value if 'Use Invoke' in self.inputs else 'False'}: col_{self.static_uid}.operator_context = "EXEC_DEFAULT"
+                    col_{self.static_uid}.operator_context = "INVOKE_DEFAULT" if {self.inputs['Use Invoke'].python_value if 'Use Invoke' in self.inputs else 'True'} else "EXEC_DEFAULT"
                     {self.indent([out.python_value if out.name == 'Column' else '' for out in self.outputs], 5)}
                     """
--- nodes/Interface/Sublayouts/Layout Row.py
+++ nodes/Interface/Sublayouts/Layout Row.py
@@ -2,14 +2,12 @@ import bpy
 from ...base_node import SN_ScriptingBaseNode
 
 
-
 class SN_LayoutRowNodeNew(SN_ScriptingBaseNode, bpy.types.Node):
-
     bl_idname = "SN_LayoutRowNodeNew"
     bl_label = "Row"
     bl_width_default = 200
     node_color = "INTERFACE"
-        
+
     def layout_type(self, socket):
         if socket.name == "Row":
             return f"row_{self.static_uid}"
@@ -27,7 +25,9 @@ class SN_LayoutRowNodeNew(SN_ScriptingBaseNode, bpy.types.Node):
         self.add_boolean_input("Use Invoke")["default_value"] = True
         self.add_float_input("Scale X")["default_value"] = 1
         self.add_float_input("Scale Y")["default_value"] = 1
-        self.add_enum_input("Alignment")["items"] = str(["Expand", "Left", "Center", "Right"])
+        self.add_enum_input("Alignment")["items"] = str(
+            ["Expand", "Left", "Center", "Right"]
+        )
         self.add_dynamic_interface_output("Row")
 
     def evaluate(self, context):
@@ -41,6 +41,6 @@ class SN_LayoutRowNodeNew(SN_ScriptingBaseNode, bpy.types.Node):
                     row_{self.static_uid}.scale_x = {self.inputs["Scale X"].python_value}
                     row_{self.static_uid}.scale_y = {self.inputs["Scale Y"].python_value}
                     row_{self.static_uid}.alignment = {self.inputs["Alignment"].python_value}.upper()
-                    if not {self.inputs['Use Invoke'].python_value if 'Use Invoke' in self.inputs else 'False'}: row_{self.static_uid}.operator_context = "EXEC_DEFAULT"
+                    row_{self.static_uid}.operator_context = "INVOKE_DEFAULT" if {self.inputs['Use Invoke'].python_value if 'Use Invoke' in self.inputs else 'True'} else "EXEC_DEFAULT"
                     {self.indent([out.python_value if out.name == 'Row' else '' for out in self.outputs], 5)}
                     """
