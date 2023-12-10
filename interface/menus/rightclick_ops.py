d7397bb7a49a898e5f3327ae8ba152eb565484ee
Joshua Knauber <joshua.knauber@gmail.com>
2023-12-10 13:58:11 +0100

    fix copy context crashes

--- interface/menus/rightclick_ops.py
+++ interface/menus/rightclick_ops.py
@@ -1,14 +1,12 @@
 import bpy
 
 
-
 REPLACE_NAMES = {
-    "ObjectBase": "bpy.data.objects['Object']", # outliner object hide
-    "LayerCollection": "bpy.context.view_layer.active_layer_collection", # outliner collection hide
-    "SpaceView3D": "bpy.context.screen.areas[0].spaces[0]", # 3d space data
-    "ToolSettings": "bpy.context.scene.tool_settings", # any space tool settings
+    "ObjectBase": "bpy.data.objects['Object']",  # outliner object hide
+    "LayerCollection": "bpy.context.view_layer.active_layer_collection",  # outliner collection hide
+    "SpaceView3D": "bpy.context.screen.areas[0].spaces[0]",  # 3d space data
+    "ToolSettings": "bpy.context.scene.tool_settings",  # any space tool settings
 }
-
 
 
 class SN_OT_CopyProperty(bpy.types.Operator):
@@ -16,7 +14,7 @@ class SN_OT_CopyProperty(bpy.types.Operator):
     bl_label = "Copy Property"
     bl_description = "Copy the path of this property"
     bl_options = {"REGISTER", "UNDO", "INTERNAL"}
-    
+
     def execute(self, context):
         # get property details
         property_pointer = getattr(context, "button_pointer", None)
@@ -25,10 +23,10 @@ class SN_OT_CopyProperty(bpy.types.Operator):
         # copy data path if available
         if bpy.ops.ui.copy_data_path_button.poll():
             bpy.ops.ui.copy_data_path_button("INVOKE_DEFAULT", full_path=True)
-            
+
             context.scene.sn.last_copied_datatype = property_value.type.title()
             path = context.window_manager.clipboard.replace('"', "'")
-            
+
             if path and path[-1] == "]" and path[:-1].split("[")[-1].isdigit():
                 path = "[".join(path.split("[")[:-1])
                 context.scene.sn.last_copied_datatype += " Vector"
@@ -36,8 +34,9 @@ class SN_OT_CopyProperty(bpy.types.Operator):
                 context.scene.sn.last_copied_datatype += " Vector"
 
             context.window_manager.clipboard = path
-            if property_value.type == "ENUM" and property_value.is_enum_flag: context.scene.sn.last_copied_datatype += " Set"
-            
+            if property_value.type == "ENUM" and property_value.is_enum_flag:
+                context.scene.sn.last_copied_datatype += " Set"
+
             context.scene.sn.last_copied_datapath = context.window_manager.clipboard
 
             self.report({"INFO"}, message="Copied!")
@@ -47,18 +46,23 @@ class SN_OT_CopyProperty(bpy.types.Operator):
         if property_pointer and property_value:
             if property_pointer.bl_rna.identifier in REPLACE_NAMES:
                 context.window_manager.clipboard = f"{REPLACE_NAMES[property_pointer.bl_rna.identifier]}.{property_value.identifier}"
-                context.window_manager.clipboard = context.window_manager.clipboard.replace('"', "'")
+                context.window_manager.clipboard = (
+                    context.window_manager.clipboard.replace('"', "'")
+                )
                 context.scene.sn.last_copied_datatype = property_value.type.title()
-                if property_value.type == "ENUM" and property_value.is_enum_flag: context.scene.sn.last_copied_datatype += " Set"
+                if property_value.type == "ENUM" and property_value.is_enum_flag:
+                    context.scene.sn.last_copied_datatype += " Set"
                 context.scene.sn.last_copied_datapath = context.window_manager.clipboard
                 self.report({"INFO"}, message="Copied!")
                 return {"FINISHED"}
 
         # error when property not available
-        self.report({"ERROR"}, message="We can't copy this property yet! Please use the Blend Data browser to find it!")
+        self.report(
+            {"ERROR"},
+            message="We can't copy this property yet! Please use the Blend Data browser to find it!",
+        )
         print("Serpens Log: ", property_pointer, property_value)
         return {"CANCELLED"}
-
 
 
 class SN_OT_CopyOperator(bpy.types.Operator):
@@ -70,17 +74,21 @@ class SN_OT_CopyOperator(bpy.types.Operator):
     def find_ops_path_from_rna(self, rna_identifier):
         for cat_name in dir(bpy.ops):
             if cat_name[0].isalpha() and not cat_name == "class":
-                try: cat = eval(f"bpy.ops.{cat_name}")
-                except: cat = None
+                try:
+                    cat = eval(f"bpy.ops.{cat_name}")
+                except:
+                    cat = None
                 if cat:
                     for op_name in dir(cat):
                         if op_name[0].isalpha():
-                            try: op = eval(f"bpy.ops.{cat_name}.{op_name}")
-                            except: op = None
+                            try:
+                                op = eval(f"bpy.ops.{cat_name}.{op_name}")
+                            except:
+                                op = None
                             if op and op.get_rna_type().identifier == rna_identifier:
                                 return f"bpy.ops.{cat_name}.{op_name}()"
         return None
-    
+
     def execute(self, context):
         # copy operator if available
         if bpy.ops.ui.copy_python_command_button.poll():
@@ -89,7 +97,7 @@ class SN_OT_CopyOperator(bpy.types.Operator):
             return {"FINISHED"}
 
         # get button details
-        button_value = getattr(context, "button_operator", None)    
+        button_value = getattr(context, "button_operator", None)
 
         # check if value exists
         if button_value:
@@ -100,12 +108,27 @@ class SN_OT_CopyOperator(bpy.types.Operator):
                 return {"FINISHED"}
 
         # error when button not available
-        self.report({"ERROR"}, message="We can't copy this operator yet! Please report this to the developers!")
+        self.report(
+            {"ERROR"},
+            message="We can't copy this operator yet! Please report this to the developers!",
+        )
         print("Serpens Log: ", button_value)
         return {"CANCELLED"}
-    
-    
-    
+
+
+def copy_context():
+    context = {}
+    for attribute in dir(bpy.context):
+        if (
+            attribute[0].isalpha()
+            and not attribute in ["property"]
+            and hasattr(bpy.context, attribute)
+            and not callable(getattr(bpy.context, attribute))
+        ):
+            context[attribute] = getattr(bpy.context, attribute)
+    return context
+
+
 class SN_OT_CopyContext(bpy.types.Operator):
     bl_idname = "sn.copy_context"
     bl_label = "Copy Context"
@@ -113,8 +136,9 @@ class SN_OT_CopyContext(bpy.types.Operator):
     bl_options = {"REGISTER", "INTERNAL"}
 
     def execute(self, context):
+        copied = copy_context()
         context.scene.sn.copied_context.clear()
-        context.scene.sn.copied_context.append(context.copy())
+        context.scene.sn.copied_context.append(copied)
         context.scene.sn.hide_preferences = True
         for screen in bpy.data.screens:
             for area in screen.areas:
