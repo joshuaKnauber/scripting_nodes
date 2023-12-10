2fb639d47082460d1fce08d3fbc1eb5b056feefc
Joshua Knauber <joshua.knauber@gmail.com>
2023-12-10 13:59:27 +0100

    fix dpi on text size node

--- nodes/Program/Modal/Text Size.py
+++ nodes/Program/Modal/Text Size.py
@@ -2,9 +2,7 @@ import bpy
 from ...base_node import SN_ScriptingBaseNode
 
 
-
 class SN_TextSizeNode(SN_ScriptingBaseNode, bpy.types.Node):
-
     bl_idname = "SN_TextSizeNode"
     bl_label = "Text Size"
     bl_width_default = 200
@@ -12,13 +10,13 @@ class SN_TextSizeNode(SN_ScriptingBaseNode, bpy.types.Node):
     def on_create(self, context):
         self.add_string_input("Text").default_value = "My Text"
         self.add_string_input("Font").subtype = "FILE_PATH"
-                
+
         self.add_float_input("Size").default_value = 20
         self.add_integer_input("DPI").default_value = 72
-        
+
         self.add_float_output("Width")
         self.add_float_output("Height")
-    
+
     def evaluate(self, context):
         self.code_imperative = f"""
             def get_text_dimensions(text, font, size, dpi):
@@ -28,14 +26,21 @@ class SN_TextSizeNode(SN_ScriptingBaseNode, bpy.types.Node):
                 if font_id == -1:
                     print("Couldn't load font!")
                 else:
-                    blf.size(font_id, size, dpi)
+                    if bpy.app.version >= (3, 4, 0):
+                        blf.size(font_id, size)
+                    else:
+                        blf.size(font_id, size, dpi)
                 return blf.dimensions(font_id, text)
         """
-        
+
         self.code_import = """
             import blf
             import os
             """
-        
-        self.outputs["Width"].python_value = f"get_text_dimensions({self.inputs['Text'].python_value}, {self.inputs['Font'].python_value}, {self.inputs['Size'].python_value}, {self.inputs['DPI'].python_value})[0]"
-        self.outputs["Height"].python_value = f"get_text_dimensions({self.inputs['Text'].python_value}, {self.inputs['Font'].python_value}, {self.inputs['Size'].python_value}, {self.inputs['DPI'].python_value})[1]"
+
+        self.outputs[
+            "Width"
+        ].python_value = f"get_text_dimensions({self.inputs['Text'].python_value}, {self.inputs['Font'].python_value}, {self.inputs['Size'].python_value}, {self.inputs['DPI'].python_value})[0]"
+        self.outputs[
+            "Height"
+        ].python_value = f"get_text_dimensions({self.inputs['Text'].python_value}, {self.inputs['Font'].python_value}, {self.inputs['Size'].python_value}, {self.inputs['DPI'].python_value})[1]"
