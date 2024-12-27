6df83deab999a8a10f3d7680708d8706104c529c
Joshua Knauber <joshua.knauber@gmail.com>
2024-12-27 14:51:42 +0100

    base node with menu

---
+++ addon/scripting_nodes/src/features/nodes/base_node.py
@@ -0,0 +1,42 @@
+from scripting_nodes.src.lib.utils.uuid import get_short_id
+from scripting_nodes.src.features.node_tree.node_tree import ScriptingNodeTree
+import bpy
+
+
+class SNA_BaseNode(bpy.types.Node):
+
+    @classmethod
+    def poll(cls, ntree):
+        """Checks if the node is valid"""
+        return ntree.bl_idname == ScriptingNodeTree.bl_idname
+
+    def ntree_poll(self, group):
+        """Checks if the node tree is valid"""
+        return group.bl_idname == ScriptingNodeTree.bl_idname
+
+    @property
+    def node_tree(self):
+        """Returns the node tree this node lives in"""
+        return self.id_data
+
+    ### Properties
+
+    is_sn = True
+
+    id: bpy.props.StringProperty(
+        default="", name="ID", description="Unique ID of the node"
+    )
+
+    ### Life Cycle
+
+    def init(self, context: bpy.types.Context):
+        """Called when the node is created"""
+        self.id = get_short_id()
+        self.on_create()
+
+    def on_create(self):
+        pass
+
+    def copy(self, node: bpy.types.Node):
+        """Called when the node is copied"""
+        self.id = get_short_id()
---
+++ addon/scripting_nodes/src/features/nodes/categories/categories.py
@@ -0,0 +1,41 @@
+import inspect
+from scripting_nodes import auto_load
+from scripting_nodes.src.features.node_tree.node_tree import ScriptingNodeTree
+import nodeitems_utils
+from nodeitems_utils import NodeCategory, NodeItem
+import os
+
+
+class ScriptingNodesCategory(NodeCategory):
+    @classmethod
+    def poll(cls, context):
+        return context.space_data.tree_type == ScriptingNodeTree.bl_idname
+
+
+def get_node_categories():
+    class_categories = {}
+    for cls in auto_load.ordered_classes:
+        if cls.bl_rna.base and cls.bl_rna.base.identifier == "Node":
+            path = os.path.dirname(inspect.getfile(cls))
+            dirs = path.split(os.sep)
+            if not dirs[-1] in class_categories:
+                class_categories[dirs[-1]] = []
+            class_categories[dirs[-1]].append(cls)
+
+    categories = []
+    for category, classes in class_categories.items():
+        items = []
+        for cls in classes:
+            items.append(NodeItem(cls.bl_idname))
+        categories.append(ScriptingNodesCategory(category, category, items=items))
+
+    return categories
+
+
+def register():
+    get_node_categories()
+    nodeitems_utils.register_node_categories("SCRIPTING_NODES", get_node_categories())
+
+
+def unregister():
+    nodeitems_utils.unregister_node_categories("SCRIPTING_NODES")
