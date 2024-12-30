aff9fc1313013a4dd19721aea86b54aa767c2601
Joshua Knauber <joshua.knauber@gmail.com>
2024-12-30 14:06:58 +0100

    separator + subpane

---
+++ addon/scripting_nodes/src/features/nodes/categories/Interface/node_separator.py
@@ -0,0 +1,28 @@
+from scripting_nodes.src.lib.utils.code.format import indent
+from scripting_nodes.src.features.nodes.base_node import ScriptingBaseNode
+import bpy
+
+
+class SNA_Node_Separator(ScriptingBaseNode, bpy.types.Node):
+    bl_idname = "SNA_Node_Separator"
+    bl_label = "Separator"
+
+    def update_line(self, context):
+        self._generate()
+
+    line: bpy.props.BoolProperty(name="Show Line", default=False, update=update_line)
+
+    def draw(self, context, layout):
+        layout.prop(self, "line", text="Show Line")
+
+    def on_create(self):
+        self.add_input("ScriptingInterfaceSocket")
+        inp = self.add_input("ScriptingFloatSocket", "Factor")
+        inp.value = 1
+        self.add_output("ScriptingInterfaceSocket")
+
+    def generate(self):
+        self.code = f"""
+            {self.inputs[0].layout}.separator(factor={self.inputs["Factor"].eval()}, type="{"LINE" if self.line else "SPACE"}")
+            {indent(self.outputs[0].eval(), 3)}
+        """
---
+++ addon/scripting_nodes/src/features/nodes/categories/Interface/node_subpanel.py
@@ -0,0 +1,35 @@
+from scripting_nodes.src.lib.utils.code.format import indent
+from scripting_nodes.src.features.nodes.base_node import ScriptingBaseNode
+import bpy
+
+
+class SNA_Node_Subpanel(ScriptingBaseNode, bpy.types.Node):
+    bl_idname = "SNA_Node_Subpanel"
+    bl_label = "Subpanel"
+
+    def on_create(self):
+        self.add_input("ScriptingInterfaceSocket")
+        inp = self.add_input("ScriptingBooleanSocket", "Default Closed")
+        inp.value = False
+        self.add_output("ScriptingInterfaceSocket", "Header")
+        self.add_output("ScriptingInterfaceSocket", "Panel")
+        self.add_output("ScriptingInterfaceSocket", "After")
+
+    def generate(self):
+        self.code_global = f"""
+            import bpy
+
+            class SNA_PT_AddonPanel_{self.id}(bpy.types.Panel):
+                bl_idname = "SNA_PT_AddonPanel_{self.id}"
+        """
+
+        self.code = f"""
+            header_{self.id}, panel_{self.id} = {self.inputs[0].layout}.panel("SNA_PT_AddonPanel_{self.id}", default_closed={self.inputs[1].eval()})
+            {indent(self.outputs["Header"].eval(), 3)}
+            if panel_{self.id}:
+                {indent(self.outputs["Panel"].eval("pass"), 4)}
+            {indent(self.outputs["After"].eval(), 3)}
+        """
+
+        self.outputs["Header"].layout = f"header_{self.id}"
+        self.outputs["Panel"].layout = f"panel_{self.id}"
