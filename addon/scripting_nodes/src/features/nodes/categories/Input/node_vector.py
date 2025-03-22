54f19652a0a06694faa412c2e4a5d298c816e81d
Corza <40785775+CoreyCorza@users.noreply.github.com>
2025-03-23 04:00:06 +1100

    new nodes & fixes

--- addon/scripting_nodes/src/features/nodes/categories/Input/node_vector.py
+++ addon/scripting_nodes/src/features/nodes/categories/Input/node_vector.py
@@ -6,58 +6,53 @@ class SNA_Node_Vector(ScriptingBaseNode, bpy.types.Node):
     bl_idname = "SNA_Node_Vector"
     bl_label = "Vector"
 
-    def update_value(self, context):
-        self._generate()
-
     def update_dimensions(self, context):
-
-        if hasattr(self, "outputs") and len(self.outputs) > 0:
-            self.outputs[0].dimension = self.dimension
+        self.outputs[0].dimension = self.dimension
         self._generate()
-
-    dimension_items = [
-        ("2", "Vec2", "Two-dimensional vector"),
-        ("3", "Vec3", "Three-dimensional vector"),
-        ("4", "Vec4", "Four-dimensional vector (with w component)"),
-    ]
 
     dimension: bpy.props.EnumProperty(
         name="Dimensions",
         description="Vector dimensions",
-        items=dimension_items,
+        items=[
+            ("2", "Vec2", "Two-dimensional vector"),
+            ("3", "Vec3", "Three-dimensional vector"),
+            ("4", "Vec4", "Four-dimensional vector (with w component)"),
+        ],
         default="3",
         update=update_dimensions,
     )
 
-    value_x: bpy.props.FloatProperty(default=0.0, update=update_value)
-    value_y: bpy.props.FloatProperty(default=0.0, update=update_value)
-    value_z: bpy.props.FloatProperty(default=0.0, update=update_value)
-    value_w: bpy.props.FloatProperty(default=0.0, update=update_value)
+    vector: bpy.props.FloatVectorProperty(
+        name="Vector",
+        size=4,
+        default=(0.0, 0.0, 0.0, 0.0),
+        update=lambda self, context: self._generate(),
+    )
 
     def draw(self, context, layout):
         layout.prop(self, "dimension", text="")
-
         col = layout.column(align=True)
-        col.prop(self, "value_x", text="")
-        col.prop(self, "value_y", text="")
+        col.prop(self, "vector", index=0, text="")
+        col.prop(self, "vector", index=1, text="")
 
         if self.dimension in ("3", "4"):
-            col.prop(self, "value_z", text="")
+            col.prop(self, "vector", index=2, text="")
 
         if self.dimension == "4":
-            col.prop(self, "value_w", text="")
+            col.prop(self, "vector", index=3, text="")
 
     def on_create(self):
         self.add_output("ScriptingVectorSocket")
-        if hasattr(self, "outputs") and len(self.outputs) > 0:
-            self.outputs[0].dimension = self.dimension
+        self.outputs[0].dimension = self.dimension
 
     def generate(self):
         if self.dimension == "2":
-            self.outputs[0].code = f"({self.value_x}, {self.value_y})"
+            self.outputs[0].code = f"({self.vector[0]}, {self.vector[1]})"
         elif self.dimension == "3":
-            self.outputs[0].code = f"({self.value_x}, {self.value_y}, {self.value_z})"
+            self.outputs[0].code = (
+                f"({self.vector[0]}, {self.vector[1]}, {self.vector[2]})"
+            )
         else:
             self.outputs[0].code = (
-                f"({self.value_x}, {self.value_y}, {self.value_z}, {self.value_w})"
+                f"({self.vector[0]}, {self.vector[1]}, {self.vector[2]}, {self.vector[3]})"
             )
