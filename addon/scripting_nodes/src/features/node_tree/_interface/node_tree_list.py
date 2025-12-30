21f5963d06b8d371c24293610d14dc9025e2af38
Joshua Knauber <joshua.knauber@gmail.com>
2025-12-30 18:25:39 +0100

    hide node groups from ui list

--- addon/scripting_nodes/src/features/node_tree/_interface/node_tree_list.py
+++ addon/scripting_nodes/src/features/node_tree/_interface/node_tree_list.py
@@ -35,5 +35,8 @@ class SNA_UL_NodeTreesList(bpy.types.UIList):
         for i, ntree in enumerate(groups):
             if not is_sn(ntree):
                 flt_flags[i] = 0
+            # Hide group node trees
+            elif getattr(ntree, "is_group", False):
+                flt_flags[i] = 0
 
         return flt_flags, flt_neworder
