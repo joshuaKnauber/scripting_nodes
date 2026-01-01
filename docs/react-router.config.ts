a8d3962d8512db7d7544720e6397d0072adfc0c2
Joshua Knauber <joshua.knauber@gmail.com>
2026-01-01 15:10:53 +0100

    deploy setup

--- docs/react-router.config.ts
+++ docs/react-router.config.ts
@@ -3,9 +3,11 @@ import fg from "fast-glob";
 import { createGetUrl, getSlugs } from "fumadocs-core/source";
 
 const getUrl = createGetUrl("/docs");
+const isProduction = process.env.NODE_ENV === "production";
 
 export default {
   ssr: true,
+  basename: isProduction ? "/scripting_nodes" : "/",
   async prerender({ getStaticPaths }) {
     const paths: string[] = [];
     const excluded: string[] = [];
